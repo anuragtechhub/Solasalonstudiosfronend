4f52d0be3a7388e77382b4134fd5a21a2fa25d5f
Jeff Bail <jeff@jeffbail.com>
2014-12-22 11:05:02 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Solasalonstudios::Application.config.secret_key_base = '8923ca21be822c186b2734c7f8271c1082d98331203ee9d537232f4d938c6f19351da7e4f55f367234cdd8f06ca9c18d2d4aadf5cd2e1c6db1b5da6850de78e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Solasalonstudios::Application.config.session_store :cookie_store, key: '_solasalonstudios_session'
