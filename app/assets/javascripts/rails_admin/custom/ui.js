a25115e115b0d87154d99cbece4d319e73366528
Jeff Bail <jeff@jeffbail.com>
2015-01-06 12:18:38 -0700

    rails admin customization

---
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -0,0 +1,8 @@
+$(document).on('rails_admin.dom_ready', function() {  
+  
+  $('.dropdown-toggle').dropdown();
+  $('#filters').hide().siblings('.dropdown-toggle').hide();
+  var $title = $('title');
+  try { $title.html($('title').html().replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); })); } catch (e) { }
+
+});
