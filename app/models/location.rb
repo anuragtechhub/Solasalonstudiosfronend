db10d445ce4ac26682064218bc81c072013e5eb3
Jeff Bail <jeff@jeffbail.com>
2015-01-08 07:58:16 -0700

    location image directory size

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,34 +6,34 @@ class Location < ActiveRecord::Base
   has_attached_file :floorplan_image
   validates_attachment_content_type :floorplan_image, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :image_1
+  has_attached_file :image_1, :styles => { :directory => '375x375#' }
   validates_attachment_content_type :image_1, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :image_2
+  has_attached_file :image_2, :styles => { :directory => '375x375#' }
   validates_attachment_content_type :image_2, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :image_3
+  has_attached_file :image_3, :styles => { :directory => '375x375#' }
   validates_attachment_content_type :image_3, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :image_4
+  has_attached_file :image_4, :styles => { :directory => '375x375#' }
   validates_attachment_content_type :image_4, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :image_5
+  has_attached_file :image_5, :styles => { :directory => '375x375#' }
   validates_attachment_content_type :image_5, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :image_6
+  has_attached_file :image_6, :styles => { :directory => '375x375#' }
   validates_attachment_content_type :image_6, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :image_7
+  has_attached_file :image_7, :styles => { :directory => '375x375#' }
   validates_attachment_content_type :image_7, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :image_8
+  has_attached_file :image_8, :styles => { :directory => '375x375#' }
   validates_attachment_content_type :image_8, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :image_9
+  has_attached_file :image_9, :styles => { :directory => '375x375#' }
   validates_attachment_content_type :image_9, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :image_10
+  has_attached_file :image_10, :styles => { :directory => '375x375#' }
   validates_attachment_content_type :image_10, :content_type => /\Aimage\/.*\Z/              
 
   validates :name, :presence => true, :uniqueness => true
