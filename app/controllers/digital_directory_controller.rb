ebbdd8551553b3db5b0ae3d210318e217c43663d
Jeff Bail <jeff@jeffbail.com>
2015-02-09 11:32:42 -0700

    more error checking for digital directory

--- app/controllers/digital_directory_controller.rb
+++ app/controllers/digital_directory_controller.rb
@@ -2,7 +2,7 @@ class DigitalDirectoryController < ApplicationController
   
   def show
     @location = Location.find_by :url_name => params[:location_url_name]
-    @stylists = @location.stylists.where('lower(status) != ?', 'closed').sort_by { |location| location.studio_number.to_i }.to_a
+    @stylists = @location.stylists.where('lower(status) = ?', 'open').sort_by { |location| location.studio_number.to_i }.to_a
 
     render :layout => 'empty'
   end
