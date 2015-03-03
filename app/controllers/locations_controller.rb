0b90f0eacabe078c50ba4e3b8f87e2b88516bbb9
Jeff Bail <jeff@jeffbail.com>
2015-03-03 09:42:04 -0700

    location by city, state and show salon

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,11 +3,30 @@ class LocationsController < PublicWebsiteController
   end
 
   def city
+    query_param = "%#{params[:city]}%"
+
+    locations1 = Location.near(params[:city])
+    locations2 = Location.where('city LIKE ?', query_param)
+    @locations = locations1 + locations2
+    if @locations
+      @locations.uniq!
+      @locations.sort! { |a, b| a.name <=> b.name }
+    end
   end
 
   def state
+    query_param = "%#{params[:state]}%"
+
+    locations1 = Location.near(params[:state])
+    locations2 = Location.where('state LIKE ?', query_param)
+    @locations = locations1 + locations2
+    if @locations
+      @locations.uniq!
+      @locations.sort! { |a, b| a.name <=> b.name }
+    end
   end
 
   def salon
+    @location = Location.find_by(:url_name => params[:url_name])
   end
 end
