31504552bdeac0864fab75e661650e7f0d744521
Jeff Bail <jeff@jeffbail.com>
2015-03-24 15:22:40 -0600

    work on responsive styling

--- app/assets/javascripts/public_website.js
+++ app/assets/javascripts/public_website.js
@@ -6,6 +6,36 @@
 
 $(function () {
 
+  var $headerNav = $('#header .nav');
+  var $window = $(window);
+
+  // expandable search
   var uiSearch = new UISearch( document.getElementById('sb-search'));
-  
+
+  // mobile top nav menu
+  $('#mobile-top-nav-button').on('click', function () {
+    
+
+    if ($headerNav.is(':visible')) {
+      $headerNav.slideUp('fast');
+    } else {
+      $headerNav.slideDown('fast');
+    }
+
+    return false;
+  });
+
+  // window resize handler
+  $window.on('resize', function () {
+    // if (uiSearch && typeof uiSearch.close === 'function') {
+    //   console.log('close')
+    //   uiSearch.close();
+    // }
+    if ($window.width() > 1000) {
+      $headerNav.show();
+    } else {
+      $headerNav.hide();
+    }
+  });
+
 });
