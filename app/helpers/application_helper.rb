876817e76494b80319f6bb84816c3261fc802578
Jeff Bail <jeff@jeffbail.com>
2015-05-07 07:59:17 -0600

    url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
   def valid_email?(email = nil)
     email && email =~ /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   end
+
+  def url_helper(url = '')
+    unless url && url.starts_with?('http')
+      url = 'http://' + url;
+    end
+  end
 end
