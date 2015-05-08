312f29e626c977a06ec06e80042ce40663b269d6
Jeff Bail <jeff@jeffbail.com>
2015-05-08 11:19:07 -0600

    pink faq hover

--- app/assets/javascripts/faq.js
+++ app/assets/javascripts/faq.js
@@ -4,7 +4,10 @@ $(function () {
     var $question = $(this);
     var $answer = $question.next('.faq-answer')
 
+    $('.faq-question').not($question).removeClass('active');
     $('.faq-answer').not($answer).slideUp('fast');
+
+    $question.addClass('active');
     $answer.slideDown('fast');
   });
 
