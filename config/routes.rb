fcd7b77ea4841fd074b38fb18c201805829a831d
Jeff Bail <jeff@jeffbail.com>
2015-05-24 10:03:28 -0600

    some route redunancy to break as few old urls as possible

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,22 @@ Solasalonstudios::Application.routes.draw do
   get '5000' => 'home#sola_5000', :as => :sola_5000
 
   get "about-us" => 'about_us#index', :as => :about_us
+  get "about" => 'about_us#index'
+  
   get "contact-us" => "contact_us#index", :as => :contact_us
+  get 'contact_us' => "contact_us#index"
+
   get "faq" => 'faq#index', :as => :faqs
   get "testimonials" => 'testimonials#index', :as => :testimonials
+  
   get 'gallery' => 'gallery#index', :as => :gallery
+  get 'gallery-photos' => 'gallery#index'
+
   get 'news' => 'news#index', :as => :news
   match "newsletter/sign-up" => 'newsletter#sign_up', :via => [:get, :post], :as => :newsletter_sign_up
   get "own-your-salon" => 'own_your_salon#index', :as => :own_your_salon
+  get 'own' => 'own_your_salon#index'
+  get 'amenities' => 'own_your_salon#index'
   
   get "request-franchising-info" => "contact_us#index", :as => :request_franchising_info
   get "tour/request-a-tour" => 'contact_us#index', :as => :request_a_tour
@@ -32,13 +41,16 @@ Solasalonstudios::Application.routes.draw do
   get "locations-fullscreen" => 'locations#fullscreen', :as => :locations_fullscreen
 
   get 'salon-professionals' => 'stylists#index', :as => :salon_professionals
+  get 'stylistsearch' => 'stylists#index'
   get 'salon-professional/:url_name' => 'stylists#show', :as => :show_salon_professional
   match 'salon-professional-send-a-message' => 'stylists#send_a_message', :via => [:get, :post], :as => :salon_professional_send_a_message
 
   get "article/:url_name" => 'article#show', :as => :show_article
+  get "readmore/:url_name" => 'article#show'
 
   get "blog" => 'blog#index', :as => :blog
   get "blog/:url_name" => 'blog#show', :as => :show_blog
+  get "blog-readmore/:url_name" => 'blog#show'
   get "blog/category/:category_url_name" => 'blog#index', :as => :blog_category
 
   get 'digital-directory/:location_url_name' => 'digital_directory#show', :via => [:get, :post], :as => :digital_directory
