649a22d95eee3e7290925ad0db2a15c5e8b540e7
Jeff Bail <jeff@jeffbail.com>
2015-05-28 13:48:51 -0600

    sola sendgrid config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Solasalonstudios::Application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app32757554@heroku.com',
+  :password => '0lv3i0042934',
+  :domain => 'getmotigo.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
