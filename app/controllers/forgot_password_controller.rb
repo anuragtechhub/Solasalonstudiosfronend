ee25ae9a4c40f5fa8a120c055d54eba3e48db067
Jeff Bail <jeff@jeffbail.com>
2015-05-29 14:12:11 -0600

    forgot password

--- app/controllers/forgot_password_controller.rb
+++ app/controllers/forgot_password_controller.rb
@@ -1,19 +1,49 @@
 class ForgotPasswordController < ApplicationController
   def form
-    if request.post?
-      p "#{params.inspect}"
-      admin = Admin.where(:email => params[:username]).first
-      @error = 'Unable to find an administrator with that username' unless admin
+    if flash[:error]
+      @error = flash[:error];
+      flash[:error] = nil;
+    end
 
-      if admin && admin.forgot_password
-        @success = "Success! We sent a forgot password email to #{admin.email_address}"
-        params[:username] = ''
+    if request.post?
+      admin = Admin.where(:email => params[:username]).first
+      
+      if params[:username].blank? and admin.blank?
+        @error = 'Please enter a username'
+      elsif admin.blank?
+        @error = "We are unable to find an administrator with the username '#{params[:username]}'"
       else
-        @error = 'There was a problem processing your forgot password request. Please try again. If the problem persists, please contact support'
+        if admin && admin.forgot_password
+          @success = "Success! We sent a forgot password email to #{admin.email_address}"
+          params[:username] = ''
+        else
+          @error = 'There was a problem processing your forgot password request. Please try again. If the problem persists, please contact support'
+        end
       end
     end
   end
 
   def reset
+    admin = Admin.where(:email => params[:username], :forgot_password_key => params[:key]).first
+    if admin
+      if request.post?
+        if params[:password].blank? || params[:password_confirmation].blank?
+          @error = 'Please fill out both the new password and new password confirmation fields'
+        elsif params[:password] != params[:password_confirmation]
+          @error = 'The passwords you entered do not match'
+        else
+          admin.password = params[:password]
+          admin.password_confirmation = params[:password_confirmation]
+          admin.forgot_password_key = nil
+          if admin.save
+            @success = "Your password was updated successfully! #{view_context.link_to 'Click here to login', :rails_admin}"
+          else
+            @error = 'There was a problem updating your password. Please try again. If the problem persists, please contact support'
+          end
+        end
+      end
+    else
+      redirect_to :forgot_password_form, :flash => { :error => "The time alloted to reset your password has expired. Please make a new forgot password request" }
+    end
   end
 end
