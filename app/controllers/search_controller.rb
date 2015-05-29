bc0337bf279d5b72dbdc423eecd4cfcc399e2b89
Jeff Bail <jeff@jeffbail.com>
2015-05-29 15:55:53 -0600

    random tweaks

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,26 +4,38 @@ class SearchController < PublicWebsiteController
 
   def results
     if params[:query]
-      query_param = "%#{params[:query].downcase.gsub(/\s/, '%')}%"
 
-      # locations
-      locations1 = Location.near(params[:query])
-      locations2 = Location.where(:status => 'open').where('LOWER(state) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param)
-      @locations = locations1.open + locations2.open
-      if @locations
-        @locations.uniq!
-        @locations.sort! { |a, b| a.name <=> b.name }
+      @locations = Location.where(:status => 'open')
+
+      params[:query] = params[:query].strip
+      params[:query].split(' ').each do |q|
+        query_param = "%#{q.downcase}%"
+
       end
 
-      # stylists
-      if params[:stylists] != 'hidden'
-        @stylists = Stylist.where(:status => 'open').where('LOWER(business_name) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param)
-        if @stylists
-          @stylist = @stylists.open
-          @stylists.uniq!
-          @stylists.sort! { |a, b| a.name <=> b.name }
-        end
-      end
+      # query_param = "%#{params[:query].downcase.gsub(/\s/, '%')}%"
+
+      # # locations
+      # locations1 = Location.near(params[:query])
+
+      # if params[:query]
+
+      # locations2 = Location.where(:status => 'open').where('LOWER(state) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param)
+      # @locations = locations1.open + locations2.open
+      # if @locations
+      #   @locations.uniq!
+      #   @locations.sort! { |a, b| a.name <=> b.name }
+      # end
+
+      # # stylists
+      # if params[:stylists] != 'hidden'
+      #   @stylists = Stylist.where(:status => 'open').where('LOWER(business_name) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param)
+      #   if @stylists
+      #     @stylist = @stylists.open
+      #     @stylists.uniq!
+      #     @stylists.sort! { |a, b| a.name <=> b.name }
+      #   end
+      # end
     end
   end
 end
