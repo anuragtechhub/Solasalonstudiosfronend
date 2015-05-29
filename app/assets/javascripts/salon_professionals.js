10ee7d16170ac8a102dfb0082a5531868c59a573
Jeff Bail <jeff@jeffbail.com>
2015-05-29 17:06:17 -0600

    heading text width

--- app/assets/javascripts/salon_professionals.js
+++ app/assets/javascripts/salon_professionals.js
@@ -31,4 +31,21 @@ $(function () {
   });
   $('#send-a-message').tooltipster({theme: 'tooltipster-noir', timer: 3000, trigger: 'foo'});
 
+  $.fn.textWidth = function(){
+    var html_org = $(this).html();
+    var html_calc = '<span>' + html_org + '</span>';
+    $(this).html(html_calc);
+    var width = $(this).find('span:first').width();
+    $(this).html(html_org);
+    return width;
+  }
+
+  // size name and business name line
+  var $name = $('.salon_professional_name');
+  var $biz = $('.salon_professional_business_name');
+  
+  if ($name.textWidth() > $biz.textWidth()) {
+    $biz.width($name.textWidth());
+  }
+
 });
