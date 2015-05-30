19d94c7782baea046d886a5fa10a4d823ac742a1
Jeff Bail <jeff@jeffbail.com>
2015-05-30 11:00:07 -0600

    dynamic sola 5000 carousel

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,44 +3,44 @@ class SearchController < PublicWebsiteController
   skip_before_filter :verify_authenticity_token
 
   def results
+    # if params[:query]
+
+    #   @locations = Location.near("%#{params[:query].downcase.gsub(/\s/, '%')}%").to_a
+    #   @stylists = []
+
+    #   params[:query] = params[:query].strip
+    #   params[:query].split(' ').each do |q|
+    #     query_param = "%#{q.downcase}%"
+
+    #     @locations.concat Location.where(:status => 'open').where('LOWER(state) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param).to_a
+    #     @stylists.concat Stylist.where(:status => 'open').where('LOWER(business_name) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param).to_a if params[:stylists] != 'hidden'
+    #   end
+
+    #   @locations = @locations.uniq.sort { |a, b| a.name <=> b.name } if @locations
+    #   @stylists = @stylists.uniq.sort { |a, b| a.name <=> b.name } if @stylists
+    # end
+
     if params[:query]
+      query_param = "%#{params[:query].downcase.gsub(/\s/, '%')}%"
 
-      @locations = Location.near("%#{params[:query].downcase.gsub(/\s/, '%')}%").to_a
-      @stylists = []
-
-      params[:query] = params[:query].strip
-      params[:query].split(' ').each do |q|
-        query_param = "%#{q.downcase}%"
-
-        @locations.concat Location.where(:status => 'open').where('LOWER(state) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param).to_a
-        @stylists.concat Stylist.where(:status => 'open').where('LOWER(business_name) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param).to_a if params[:stylists] != 'hidden'
+      # locations
+      locations1 = Location.near(params[:query])
+      locations2 = Location.where(:status => 'open').where('LOWER(state) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param)
+      @locations = locations1.open + locations2.open
+      if @locations
+        @locations.uniq!
+        @locations.sort! { |a, b| a.name <=> b.name }
       end
 
-      @locations = @locations.uniq.sort { |a, b| a.name <=> b.name } if @locations
-      @stylists = @stylists.uniq.sort { |a, b| a.name <=> b.name } if @stylists
+      # stylists
+      if params[:stylists] != 'hidden'
+        @stylists = Stylist.where(:status => 'open').where('LOWER(business_name) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param)
+        if @stylists
+          @stylist = @stylists.open
+          @stylists.uniq!
+          @stylists.sort! { |a, b| a.name <=> b.name }
+        end
+      end
     end
-
-    # if params[:query]
-    #   query_param = "%#{params[:query].downcase.gsub(/\s/, '%')}%"
-
-    #   # locations
-    #   locations1 = Location.near(params[:query])
-    #   locations2 = Location.where(:status => 'open').where('LOWER(state) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param)
-    #   @locations = locations1.open + locations2.open
-    #   if @locations
-    #     @locations.uniq!
-    #     @locations.sort! { |a, b| a.name <=> b.name }
-    #   end
-
-    #   # stylists
-    #   if params[:stylists] != 'hidden'
-    #     @stylists = Stylist.where(:status => 'open').where('LOWER(business_name) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param)
-    #     if @stylists
-    #       @stylist = @stylists.open
-    #       @stylists.uniq!
-    #       @stylists.sort! { |a, b| a.name <=> b.name }
-    #     end
-    #   end
-    # end
   end
 end
