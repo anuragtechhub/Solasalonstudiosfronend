83854355bc5a1a747afb00a8f505638c7bf1c6fb
Jeff Bail <jeff@jeffbail.com>
2015-06-04 16:24:20 -0600

    url helper fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
 
+  require 'uri'
+
   def valid_email?(email = nil)
     email && email =~ /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  end
+
+  def valid_url?(url = nil)
+    url && URI.parse(url)
+  rescue URI::InvalidURIError
+    false
   end
 
   def url_helper(url = '')
