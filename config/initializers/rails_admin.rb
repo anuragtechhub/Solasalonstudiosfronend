95ef0dcb5c955a0633dd13d245b200022c71b2e9
Jeff Bail <jeff@jeffbail.com>
2015-06-24 10:24:05 -0600

    social buttons for salon pages

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -315,8 +315,8 @@ RailsAdmin.config do |config|
       end
       group :social do
         field :facebook_url
-        #field :instagram_url
-        #field :pinterest_url
+        field :instagram_url
+        field :pinterest_url
         field :twitter_url
         #field :yelp_url
       end
@@ -554,8 +554,8 @@ RailsAdmin.config do |config|
       group :social do
         active false
         field :facebook_url
-        #field :instagram_url
-        #field :pinterest_url
+        field :instagram_url
+        field :pinterest_url
         field :twitter_url
         #field :yelp_url
       end
