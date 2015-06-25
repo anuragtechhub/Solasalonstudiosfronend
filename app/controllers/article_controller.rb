5045cec1042ad8f520b6164fed728fd9ef5b6365
Jeff Bail <jeff@jeffbail.com>
2015-06-25 15:50:37 -0600

    news and pr

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,5 +1,6 @@
 class ArticleController < PublicWebsiteController
   def show
-    @article = Article.find_by(:url_name => params[:url_name])
+    #@article = Article.find_by(:url_name => params[:url_name])
+    redirect_to :news
   end
 end
