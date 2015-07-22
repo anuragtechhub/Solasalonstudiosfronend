1edf4c323ea5ae3f4201ab080509b2413bc577b0
Jeff Bail <jeff@jeffbail.com>
2015-07-22 08:25:42 -0600

    application x-frame header

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ ENV['RAILS_ADMIN_THEME'] = 'custom'
 
 module Solasalonstudios
   class Application < Rails::Application
+
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
     
     config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
       r301 %r{^/printing.*$}, 'https://www.conquestgraphics.com/login?ATN=SolaSalon'
