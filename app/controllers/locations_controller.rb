e07243cdfb2ca8182c27cfb8272d5151e634fb41
Jeff Bail <jeff@jeffbail.com>
2015-07-23 17:18:57 -0600

    print

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -37,6 +37,7 @@ class LocationsController < PublicWebsiteController
   end
 
   def state
+    p "locations by state #{params[:state]}"
     @all_locations = Location.where(:status => 'open')
     query_param = "%#{params[:state]}%"
     @locations = Location.where(:status => 'open').where('state LIKE ?', query_param)
