3c091232854ac8b5fea0e2d3846ade8c198728cc
Jeff Bail <jeff@jeffbail.com>
2015-11-23 15:46:33 -0700

    sola s3 creds

--- config/application.rb
+++ config/application.rb
@@ -64,6 +64,6 @@ module Solasalonstudios
     config.assets.precompile += ['digital_directory.css', 'digital_directory.js', 'ckeditor/*']
     config.assets.precompile += ['public_website.css', 'public_website.js', 'locations.js', 'locations_state_select.js', 'salon_stylists.js', 'blog.js', 'contact_us.js', 'own_your_salon.js', 'salon_professionals.js', 'faq.js', 'sola5000.js']
 
-    config.paperclip_defaults = {:storage => :s3, :s3_credentials => {:bucket => 'solasalonstudios', :access_key_id => 'AKIAJAKSXVOSIU7IYOTA', :secret_access_key => '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'}}
+    config.paperclip_defaults = {:storage => :s3, :s3_credentials => {:bucket => 'solasalonstudios', :access_key_id => 'AKIAIHSFVBTYHNG2XTLA', :secret_access_key => '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'}}
   end
 end
