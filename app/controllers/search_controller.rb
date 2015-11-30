42230060a69ceff80b9f98befcb028ef928fc354
Jeff Bail <jeff@jeffbail.com>
2015-11-30 10:18:41 -0700

    case insensitive search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -24,7 +24,7 @@ class SearchController < PublicWebsiteController
       query_param = "%#{params[:query].downcase.gsub(/\s/, '%')}%"
 
       # locations
-      locations1 = Location.near(params[:query])
+      locations1 = Location.near(params[:query].downcase)
       locations2 = Location.where(:status => 'open').where('LOWER(state) LIKE ? OR LOWER(city) LIKE ? OR LOWER(name) LIKE ? OR LOWER(url_name) LIKE ?', query_param, query_param, query_param, query_param)
       @locations = locations1.open + locations2.open
       if @locations
