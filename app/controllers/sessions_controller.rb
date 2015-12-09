c64405416cd66c7ec85e69de54f2411e357a156e
Jeff Bail <jeff@jeffbail.com>
2015-12-09 16:52:01 -0700

    sola session for denver

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < PublicWebsiteController
 
   def index
-    redirect_to :home
+    #redirect_to :home
+    render 'denver'
   end
 
   def denver
-    redirect_to :home
+    #redirect_to :home
   end
 
 end
