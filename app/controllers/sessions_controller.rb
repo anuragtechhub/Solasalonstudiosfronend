769d07a6086f7da096193e08045b46a442aed554
Jeff Bail <jeff@jeffbail.com>
2016-02-24 10:59:01 -0700

    minneapolis session page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,14 @@ class SessionsController < PublicWebsiteController
 
   def index
     #redirect_to :home
-    render 'denver'
+    render 'minneapolis'
   end
 
   def denver
+    #redirect_to :home
+  end
+
+  def minneapolis
     #redirect_to :home
   end
 
