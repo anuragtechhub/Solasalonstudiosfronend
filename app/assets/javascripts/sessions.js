f83de7867e8062cb0fbc840b88a5c45c9b9a23de
Jeff Bail <jeff@jeffbail.com>
2016-06-10 12:54:15 -0600

    testimonial sizing

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -18,6 +18,27 @@ $(function () {
     $(document.body).animate({scrollTop: scrollTop});
   });
 
+  // testimonial sizing
+  var resizeId;
+  $(window).on('resize.testimonial', function() {
+    clearTimeout(resizeId);
+    resizeId = setTimeout(doneResizing, 500);
+  }).trigger('resize');
+   
+  function doneResizing() {
+    var largest = 0;
+
+    $('.testimonials .owl-item').css('height','auto').each(function () {
+      var height = this.scrollHeight * 2;
+      
+      if (height > largest) {
+        largest = height;
+      }
+    });
+    
+    $('.testimonials .owl-item').height(largest);
+  };
+
   // galleries
 
   $('#view-denver-gallery').click(function () {
