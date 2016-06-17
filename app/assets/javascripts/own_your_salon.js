3ef463c8db3327cca36da56fa2baca845e2599f6
Jeff Bail <jeff@jeffbail.com>
2016-06-17 14:43:51 -0600

    incremental work on OYS

--- app/assets/javascripts/own_your_salon.js
+++ app/assets/javascripts/own_your_salon.js
@@ -7,4 +7,9 @@ $(function () {
     $this.tooltipster({theme: 'tooltipster-hughes', content: $($this.data('tooltip'))});
   });
 
+  $('.hero-tabs a').on('click', function () {
+    $('.hero-tab-content').hide().filter('[data-ref=' + $(this).attr('href') + ']').show();
+    return false;
+  });
+
 });
