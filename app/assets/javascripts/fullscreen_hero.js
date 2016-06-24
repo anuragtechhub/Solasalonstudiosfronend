e157fe87f687346b5f809bbad5730b1d2527106c
Jeff Bail <jeff@jeffbail.com>
2016-06-24 12:44:20 -0600

    fullscreen hero

--- app/assets/javascripts/fullscreen_hero.js
+++ app/assets/javascripts/fullscreen_hero.js
@@ -5,10 +5,6 @@
   var $top_header = $('#top-header');
 
   function fullscreenImage() {
-    // console.log('window', $window.width(), $window.height());
-    // console.log('$header', $header.height());
-    // console.log('$top_header', $top_header.height());
-
     $('.hero-carousel-fullscreen .item').each(function () {
       var $item = $(this);
       var $image = $item.find('img.fullscreen');
@@ -16,53 +12,20 @@
       var idealHeight = $window.height() - $header.height() - $top_header.height();
       var idealWidth = $window.width();
 
-      //console.log('idealDimensions', idealWidth, idealHeight);
-
       $item.width(idealWidth).height(idealHeight);
-      //$image.width(idealWidth).height(idealHeight);
 
-      //$image.on('load', function () {
-        var imageWidth = $image.data('width');
-        var imageHeight = $image.data('height');
-        var aspectRatio = imageWidth / imageHeight;
-
-        var ratio = idealHeight / imageHeight;
-        
-        var newWidth = idealWidth * ratio;
-        var newLeft = ((idealWidth - newWidth) / 2) + 'px';
-
-        console.log($image.attr('src'), 'aspectRatio', aspectRatio, 'ratio', ratio);
-
-        if ($image.prop('naturalWidth') < idealWidth) {
-          console.log('must increase width');
-        } else {
-          console.log('must decrease width');
-        }
-
-        if ($image.prop('naturalHeight') < idealHeight) {
-          console.log('must increase height');
-        } else {
-          console.log('must decrease height');
-        }
-
-        var heightDiff = Math.abs(idealHeight - imageHeight);
-        var widthDiff = Math.abs(idealWidth - imageWidth);
-
-        if (heightDiff > widthDiff) {
-          console.log('need to increase height more than width', newWidth * aspectRatio);
-          //newWidth = newWidth * aspectRatio;
-          //newLeft = 0;
-        } else {
-          console.log('need to increase width more than height');
-        }
-
-        console.log(imageHeight, imageWidth, newWidth, newLeft);
-        
-        $image.width(newWidth).height(idealHeight).css('left', newLeft);
-      //});
+      var imageWidth = $image.data('width');
+      var imageHeight = $image.data('height');
+      var aspectRatio = imageWidth / imageHeight;
       
+      var newWidth = idealHeight * aspectRatio;
+      var newLeft = -Math.abs((newWidth - idealWidth) / 2);
+
+      $image.width(newWidth).height(idealHeight).css('left', newLeft);
     });
   };
 
   fullscreenImage();
+
+  $window.on('resize', fullscreenImage);
 })();
