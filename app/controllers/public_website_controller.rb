31bb053bf92b398ad619c7a0adb20f3167a03540
Jeff Bail <jeff@jeffbail.com>
2016-09-06 12:42:34 -0600

    password protect canada

--- app/controllers/public_website_controller.rb
+++ app/controllers/public_website_controller.rb
@@ -5,7 +5,17 @@ class PublicWebsiteController < ApplicationController
 
   layout 'public_website'
 
-  before_action :set_locale
+  before_action :set_locale, :auth_if_canada
+
+  #http_basic_authenticate_with :name => "ohcanada", :password => "tragicallyhip", :if => 
+
+  def auth_if_canada
+    if I18n.locale != :en
+      authenticate_or_request_with_http_basic 'canada' do |name, password|
+        name == 'ocanada' && password == 'trudeau'
+      end
+    end
+  end
 
   def set_locale
     if request.domain == 'solasalonstudios.ca'
