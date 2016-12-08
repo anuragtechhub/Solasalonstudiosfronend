f08e7131f4564d289f93ec2ab41b2d34e753ce70
Jeff Bail <jeff@jeffbail.com>
2016-12-08 12:16:26 -0700

    scroll down page to video if press first button

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -39,4 +39,18 @@ $(function () {
     $('.testimonials .owl-item').height(largest);
   };
 
+  // animated scroll
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 });
