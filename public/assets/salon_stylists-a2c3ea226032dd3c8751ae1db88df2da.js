9629de944610510eb99901efa51fa2fd86b10c8b
Jeff Bail <jeff@jeffbail.com>
2017-01-09 11:45:27 -0700

    compile assets

---
+++ public/assets/salon_stylists-a2c3ea226032dd3c8751ae1db88df2da.js
@@ -0,0 +1,31 @@
+$(function () {
+
+  // sola state select
+  $('.sola-select').each(function () {
+    var $select = $(this);
+    var $placeholder = $select.find('.option-placeholder');
+    var $arrow = $select.find('.arrow');
+    var $options = $select.find('.options');
+
+    $select.find('.option-placeholder, .arrow').on('click', function () {
+      if ($options.is(':visible')) {
+        $options.hide();
+        $(window).off('click.solaselect');
+      } else {
+        $options.show();
+        $(window).on('click.solaselect', function () {
+          $options.hide();
+        });
+      }
+      return false;
+    });
+
+    $select.find('.option').on('click', function (event) {
+      window.location.href = $(event.target).data('value');
+      return false;
+    });
+
+  });
+
+
+});
