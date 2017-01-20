18b8c7825e5d0a1383b0c40cc48c1bf229c34734
Jeff Bail <jeff@jeffbail.com>
2017-01-19 20:26:19 -0700

    my sola copy

--- app/assets/javascripts/components/MySola.js.jsx
+++ app/assets/javascripts/components/MySola.js.jsx
@@ -3,8 +3,20 @@ var MySola = React.createClass({
   render: function () {
     return (
       <div className="my-sola">
-        <div className="container">#MySola</div>
+        <div className="container">
+          {this.renderHeaderCopy()}
+        </div>
         <Dropzone />
+      </div>
+    );
+  },
+
+  renderHeaderCopy: function () {
+    return (
+      <div className="header-copy">
+        <div className="overview">Overview:</div>
+        <h2>What does your Sola mean to you?</h2>
+        <p>Upload a photo and fill in one of the two statements below to create a custom image. Share your pic and tell the world why you decided to go Sola.</p>
       </div>
     );
   },
