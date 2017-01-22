0b35c6e939c504d91684ddf85eb8fcc5f7950e50
Jeff Bail <jeff@jeffbail.com>
2017-01-22 11:06:12 -0700

    incremental work on mysola image creation

--- app/controllers/my_sola_controller.rb
+++ app/controllers/my_sola_controller.rb
@@ -17,8 +17,21 @@ class MySolaController < PublicWebsiteController
     #@my_sola_image.image = params[:file]
 
     m_image = Magick::Image.read(params[:file].open).first.resize_to_fill!(1080, 1080)
+
+    # text
+    text = Magick::Draw.new
+    m_image.annotate(text, 1080, 100, 0, 300, "#MySola is my") do
+      text.font = "#{Rails.root}/lib/fonts/Lato-Regular.ttf"
+      text.gravity = Magick::NorthGravity
+      text.pointsize = 100
+      text.fill = '#ffffff'
+      text.font_weight = Magick::BoldWeight
+    end
+
+    # logo
     m_logo = Magick::Image.read(Rails.root.join('app/assets/images/logo_white.png')).first
     m_combined = m_image.composite(m_logo, 860, 940, Magick::OverCompositeOp)
+
 
     @my_sola_image.image = StringIO.open(m_combined.to_blob)
     @my_sola_image.save
