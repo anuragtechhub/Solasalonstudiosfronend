c6246f6d78df178a24be6e6d30fa2cb04e8d6cdd
Jeff Bail <jeff@jeffbail.com>
2017-01-22 14:58:31 -0700

    remove logging and save variant as it is changed

--- app/controllers/my_sola_controller.rb
+++ app/controllers/my_sola_controller.rb
@@ -10,11 +10,14 @@ class MySolaController < PublicWebsiteController
 
   def image
     @my_sola_image = MySolaImage.find(params[:id])
+    @my_sola_image.statement = params[:statement]
+    @my_sola_image.statement_variant = params[:statement_variant]
+    @my_sola_image.save
+    
     m_image = Magick::Image.read(@my_sola_image.image.url(:instagram)).first
 
     # #MySola is [BLANK]
     if params[:statement].present? && params[:statement_variant] == 'mysola_is'
-      p "DOING MYSOLA IS"
       text = Magick::Draw.new
       m_image.annotate(text, 1080, 100, 0, 300, "#MySola is my") do
         text.font = "#{Rails.root}/lib/fonts/Lato-Regular.ttf"
@@ -34,7 +37,6 @@ class MySolaController < PublicWebsiteController
 
     # I feel [BLANK] in #MySola
     if params[:statement].present? && params[:statement_variant] == 'i_feel'
-      p "DOING I FEEL"
       top_text = Magick::Draw.new
       m_image.annotate(top_text, 1080, 100, 0, 300, "I feel") do
         top_text.font = "#{Rails.root}/lib/fonts/Lato-Regular.ttf"
@@ -69,25 +71,6 @@ class MySolaController < PublicWebsiteController
   def image_upload
     @my_sola_image = MySolaImage.create
     @my_sola_image.image = params[:file]
-
-    # m_image = Magick::Image.read(params[:file].open).first.resize_to_fill!(1080, 1080)
-
-    # # text
-    # text = Magick::Draw.new
-    # m_image.annotate(text, 1080, 100, 0, 300, "#MySola is my") do
-    #   text.font = "#{Rails.root}/lib/fonts/Lato-Regular.ttf"
-    #   text.gravity = Magick::NorthGravity
-    #   text.pointsize = 100
-    #   text.fill = '#ffffff'
-    #   text.font_weight = Magick::BoldWeight
-    # end
-
-    # # logo
-    # m_logo = Magick::Image.read(Rails.root.join('app/assets/images/logo_white.png')).first
-    # m_combined = m_image.composite(m_logo, 860, 940, Magick::OverCompositeOp)
-
-
-    # @my_sola_image.image = StringIO.open(m_combined.to_blob)
     @my_sola_image.save
 
     render :json => @my_sola_image
