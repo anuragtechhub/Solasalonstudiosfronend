6b1ccb0d3969fd81b8493af1c5b24267ad2f3486
Jeff Bail <jeff@jeffbail.com>
2017-02-09 09:41:12 -0700

    funsize

--- app/models/my_sola_image.rb
+++ app/models/my_sola_image.rb
@@ -9,7 +9,7 @@ class MySolaImage < ActiveRecord::Base
   attr_accessor :delete_image
   before_validation { self.image.destroy if self.delete_image == '1' }
 
-  has_attached_file :generated_image, :source_file_options => {:all => '-auto-orient'}
+  has_attached_file :generated_image, :styles => { :funsize => '400x>' }, :source_file_options => {:all => '-auto-orient'}
   validates_attachment_content_type :generated_image, :content_type => /\Aimage\/.*\Z/
   attr_accessor :delete_generated_image
   before_validation { self.generated_image.destroy if self.delete_generated_image == '1' }
