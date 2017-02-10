383fad9018d55b5ff1ca596300453212a29ad4e5
Jeff Bail <jeff@jeffbail.com>
2017-02-10 16:48:58 -0700

    image gallery rollovers

--- app/assets/javascripts/components/MySolaGallery.js.jsx
+++ app/assets/javascripts/components/MySolaGallery.js.jsx
@@ -1,22 +1,23 @@
 var MySolaGallery = React.createClass({
 
   getInitialState: function () {
+    var results_per_page = 8;
     var images = this.props.images ? this.shuffle(JSON.parse(this.props.images)) : [];
     var images_length = images.length;
-    var total_pages = images_length / 12 + (images_length % 12 > 0 ? 1 : 0) - 1;
+    var total_pages = images_length / results_per_page + (images_length % results_per_page > 0 ? 1 : 0) - 1;
     total_pages = total_pages < 1 ? 1 : total_pages;
     
     return {
       current_page: 0,
       images: images,
       loading: false,
-      results_per_page: 12,
+      results_per_page: results_per_page,
       total_pages: total_pages,
     };
   },
 
   render: function () {
-    console.log('images.length', this.state.images.length, 'total_pages', this.state.total_pages);
+    //console.log('RENDER --- images.length', this.state.images.length, 'total_pages', this.state.total_pages);
 
     return (
       <div className="my-sola-image-gallery">
@@ -27,26 +28,67 @@ var MySolaGallery = React.createClass({
   },
 
   renderImages: function () {
-    var current_page_of_images = this.state.images.slice(this.state.current_page * this.state.results_per_page, (this.state.current_page * this.state.results_per_page) + this.state.results_per_page);
-    var images = current_page_of_images.map(function (image) {
+    var self = this;
+    var current_page_of_images = this.state.images.slice(0, (this.state.current_page * this.state.results_per_page) + this.state.results_per_page);
+    var images = current_page_of_images.map(function (image, idx) {
       return (
-        <div className="image-wrapper" key={image.id}>
+        <div className="image-wrapper" key={image.id} onMouseEnter={self.showOverlay.bind(null, image.id, idx)} onMouseLeave={self.hideOverlay.bind(null, image.id, idx)} onClick={self.toggleOverlay.bind(null, image.id, idx)}>
           <img src={image.generated_image_url} />
+          {self.renderImageOverlay(image)}
         </div>
       );
     });
 
     return (
-      <div className="images-wrapper">{images}</div>
+      <div className="images-wrapper clearfix">{images}</div>
     );
   },
 
+  renderImageOverlay: function (image) {
+    if (image && (image.instagram_handle || image.name) && image.overlay) {
+      return (
+        <a href={"https://www.instagram.com/" + (image.instagram_handle ? image.instagram_handle.substring(1) : '')} target="_blank" className="overlay">
+         <div className="handle-wrapper"><div className="handle">{image.instagram_handle || image.name}</div></div>
+        </a>
+      );
+    }
+  },
+
   renderLoadMore: function () {
-    return (
-      <div className="text-center load-more-wrapper">
-        <button type="button" className="button">LOAD MORE</button>
-      </div>
-    );
+    if (this.state.total_pages > this.state.current_page + 1) {
+      return (
+        <div className="text-center load-more-wrapper">
+          <button type="button" className="button" onClick={this.nextPage}>LOAD MORE</button>
+        </div>
+      );
+    }
+  },
+
+  nextPage: function () {
+    this.setState({current_page: this.state.current_page + 1});
+  },
+
+  showOverlay: function (id, idx) {
+    //console.log('showOverlay', idx, this.state.images[idx]);
+    this.state.images[idx].overlay = true;
+    this.forceUpdate();
+  },
+
+  hideOverlay: function (id, idx) {
+    //console.log('hideOverlay', idx, this.state.images[idx]);
+    this.state.images[idx].overlay = false;
+    this.forceUpdate();
+  },
+
+  toggleOverlay: function (id, idx) {
+    //console.log('toggleOverlay', idx, this.state.images[idx]);
+    if (this.state.images[idx].overlay) {
+      this.state.images[idx].overlay = true;
+    } else {
+      this.state.images[idx].overlay = false;
+    }
+    
+    this.forceUpdate();
   },
 
   shuffle: function (array) {
