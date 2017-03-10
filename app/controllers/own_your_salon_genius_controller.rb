a3f1768fac2a4b686284a4210635638544adcd8d
Jeff Bail <jeff@jeffbail.com>
2017-03-09 17:38:19 -0700

    incremental work

--- app/controllers/own_your_salon_genius_controller.rb
+++ app/controllers/own_your_salon_genius_controller.rb
@@ -1,7 +1,17 @@
 class OwnYourSalonGeniusController < PublicWebsiteController
   
+  before_filter :authenticate
+
   def index
 
+  end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic 'solagenius' do |name, password|
+      name == 'sola' && password == 'genius'
+    end
   end
 
 end
