36742f2b8ec1d19f174e046c2673dc11f39ecee1
Jeff Bail <jeff@jeffbail.com>
2017-04-17 09:55:19 -0600

    mysola features and mysola sliding bottom bar

--- app/assets/javascripts/mysola.js
+++ app/assets/javascripts/mysola.js
@@ -1,7 +1,7 @@
 $(function () {
 
   // tabs
-  $('a.mysola-tab').on('click', function () {
+  $('a.mysola-tab').on('click.tabs', function (e) {
     var $this = $(this);
 
     // link active
@@ -12,7 +12,29 @@ $(function () {
     $('.mysola-tab-content').hide();
     $('.mysola-tab-content[data-tab=' + $this.data('tab') + ']').show();
 
-    return false;
+    e.preventDefault();
+  });
+
+  // magic_line
+  var $main_nav = $(".mysola-tabs-container");
+  $main_nav.append("<div id='magic-line'></div>");
+  var $magic_line = $("#magic-line");
+
+  // resize handler
+  $(window).on('resize.magicline', function () {
+    $magic_line.css('left', $main_nav.find('a.active').offset().left);
+    $magic_line.width($main_nav.find('a.active').outerWidth());
+  }).trigger('resize.magicline');
+
+  // click handler
+  $('a.mysola-tab').on('click.magicline', function (e) {
+    // update magic_line width
+    $magic_line.stop().animate({
+      left: $main_nav.find('a.active').offset().left,
+      width: $main_nav.find('a.active').outerWidth()
+    });
+
+    e.preventDefault();
   });
 
 });
