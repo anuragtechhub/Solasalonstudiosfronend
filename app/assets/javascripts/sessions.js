e0989d95a62d001cfb8010114dd0d1b4ee122199
Jeff Bail <jeff@jeffbail.com>
2017-05-10 10:22:44 -0600

    turn off testimonial resizing

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -19,25 +19,25 @@ $(function () {
   });
 
   // testimonial sizing
-  var resizeId;
-  $(window).on('resize.testimonial', function() {
-    clearTimeout(resizeId);
-    resizeId = setTimeout(doneResizing, 500);
-  }).trigger('resize');
+  // var resizeId;
+  // $(window).on('resize.testimonial', function() {
+  //   clearTimeout(resizeId);
+  //   resizeId = setTimeout(doneResizing, 500);
+  // }).trigger('resize');
    
-  function doneResizing() {
-    var largest = 0;
+  // function doneResizing() {
+  //   var largest = 0;
 
-    $('.testimonials .owl-item').css('height','auto').each(function () {
-      var height = this.scrollHeight * 2;
+  //   $('.testimonials .owl-item').css('height','auto').each(function () {
+  //     var height = this.scrollHeight * 2;
       
-      if (height > largest) {
-        largest = height;
-      }
-    });
+  //     if (height > largest) {
+  //       largest = height;
+  //     }
+  //   });
     
-    $('.testimonials .owl-item').height(largest);
-  };
+  //   $('.testimonials .owl-item').height(largest);
+  // };
 
   // animated scroll
   $('a[href*="#"]:not([href="#"])').click(function() {
