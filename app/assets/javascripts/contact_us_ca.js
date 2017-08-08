84358688ee6a683c4c5baf3a172664e1d6fe1839
Jeff Bail <jeff@jeffbail.com>
2017-08-08 15:09:22 -0600

    remove canada coming soon location

--- app/assets/javascripts/contact_us_ca.js
+++ app/assets/javascripts/contact_us_ca.js
@@ -1,6 +1,6 @@
 $(function () {
   $('.select-a-state .option').click();
-  $('.select-a-location .option').click();
+  //$('.select-a-location .option').click();
 
   //contact us thank you handler for canada...
   window.contactUsFormSuccessHandler = function (data) {
