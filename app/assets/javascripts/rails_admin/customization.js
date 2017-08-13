a688c47e7b8bf1c1f195b7aa1ecc6e238956749b
Jeff Bail <jeff@jeffbail.com>
2017-08-13 11:19:04 -0600

    init commit rentmanager rake tasks

--- app/assets/javascripts/rails_admin/customization.js
+++ app/assets/javascripts/rails_admin/customization.js
@@ -2,14 +2,6 @@
 //= require autonumeric
 //= require currency_input
 
-// $(function () {
-//   $('.currency-input').currencyinput();
-// });
-
 $(document).on('ready pjax:success', function() {
   $('.currency-input').currencyinput();
 });
-
-// $(document).on('turbolinks:load', function() {
-//   $('.currency-input').currencyinput();
-// });
