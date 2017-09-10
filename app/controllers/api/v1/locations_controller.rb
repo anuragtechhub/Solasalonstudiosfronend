5223fb836dba32a314973ea8356438a1255db284
Jeff Bail <jeff@jeffbail.com>
2017-09-09 18:16:22 -0600

    remove digital directory images

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::LocationsController < ApiController
 
   def show
     @location = Location.find_by(:id => params[:id])
-    render :json => Oj.dump(@location.as_json(:methods => [:floorplan_image_url, :directory_image_1_url, :directory_image_2_url, :directory_image_3_url, :directory_image_4_url, :directory_image_5_url, :directory_image_6_url, :directory_image_7_url, :directory_image_8_url, :directory_image_9_url, :directory_image_10_url, :directory_image_11_url, :directory_image_12_url, :salon_professionals], :only => [:id, :name, :general_contact_name, :email_address_for_inquiries, :phone_number]))
+    render :json => Oj.dump(@location.as_json(:methods => [:salon_professionals], :only => [:id, :name, :general_contact_name, :email_address_for_inquiries, :phone_number]))
   end
 
 end
