ccf8d531c7c475a9b9edc700e9f56638ce262bc5
Jeff Bail <jeff@jeffbail.com>
2017-12-14 12:18:31 -0700

    sliding mobile submenu

--- app/assets/javascripts/public_website/nav.js
+++ app/assets/javascripts/public_website/nav.js
@@ -3,6 +3,8 @@ $(function () {
 	var $mobileNavWrapper = $('.mobile-nav-wrapper');
 	var $mobileNavMenu = $mobileNavWrapper.find('.mobile-nav');
 
+
+
   /**
   * Mobile top nav hamburger menu
   */
@@ -28,7 +30,6 @@ $(function () {
 
 
 
-
   /**
   * Close sliding mobile nav menus
   */
@@ -37,10 +38,60 @@ $(function () {
     //if ($mobileNavWrapper.is(':visible')) {
       $mobileNavMenu.removeClass('open');
       setTimeout(function () {
-      	$mobileNavWrapper.fadeOut('fast');
+      	$mobileNavWrapper.fadeOut('fast', function () {
+          // reset submenus
+          $mobileNavMenu.find('.sliding-submenu').removeClass('open');
+          $mobileNavMenu.find('.mobile-nav-top-menu').addClass('open');
+        });
       }, 150);
       $('body').off('touchmove.mobileNav').removeClass('stop-scrolling');
     //}
   });
+
+
+
+  /**
+  * Sliding mobile submenus
+  */
+  $mobileNavMenu.find('li > ul').each(function () {
+    var $child_ul = $(this);
+    var $parent_li = $child_ul.parent('li');
+
+    $child_ul.addClass('sliding-submenu');
+    $mobileNavMenu.append($child_ul);
+
+    // clicking parent to open child
+    $parent_li.find('> a').on('click', function () {
+      var $this = $(this);
+      var $parent = $this.parents('ul');
+      var $submenu = $mobileNavMenu.find('.sliding-submenu[data-submenu="' + $this.data('submenu') + '"]');
+
+      console.log('you clicked????', $parent, $submenu);
+
+      if ($parent.hasClass('open')) {
+        console.log('adding open to submenu')
+        $parent.removeClass('open');
+        $submenu.addClass('open');
+      } else {
+        console.log('removing open from submenu')
+        $parent.addClass('open');
+        $submenu.removeClass('open');
+      }
+      
+      return false;
+    });
+
+    // clicking child to open parent
+    $mobileNavMenu.find('.sliding-submenu a[data-menu]').each(function () {
+      var $submenu_a = $(this);
+
+      $submenu_a.on('click', function () {
+        $('ul[data-menu="' + $submenu_a.data('menu') + '"').addClass('open');
+        $submenu_a.parents('[data-submenu]').removeClass('open');
+        return false;
+      })
+    })
+  });
+
 
 });
