da0a0f4a5e09d323c488465abeb699df53989672
Jeff Bail <jeff@jeffbail.com>
2017-12-21 14:09:33 -0700

    home services animation

---
+++ app/assets/javascripts/public_website/home.js
@@ -0,0 +1,22 @@
+$(function () {
+
+	/**
+	* Animated service heading
+	*/
+	function animateToNextService() {
+		var $services = $('.home-services-animation span');
+		var $active = $services.filter('.active');
+		var active_index = $services.index($active);
+
+		if (active_index >= $services.length - 1) {
+			$active.removeClass('active');
+			$services.eq(0).addClass('active');
+		} else {
+			$active.removeClass('active');
+			$services.eq(active_index + 1).addClass('active');
+		}
+	};
+
+	setInterval(animateToNextService, 3000);
+
+});
