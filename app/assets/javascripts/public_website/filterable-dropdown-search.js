d5fe343a80e3b29333a4edcb75c3508b6ff53c13
Jeff Bail <jeff@jeffbail.com>
2017-12-21 21:57:42 -0700

    home search

--- app/assets/javascripts/public_website/filterable-dropdown-search.js
+++ app/assets/javascripts/public_website/filterable-dropdown-search.js
@@ -3,8 +3,6 @@ $.widget('solasalonstudios.filterabledropdownsearch', {
   _create: function() {
     var self = this;
 
-    console.log('create filterable dropdown search yo!');
-
     self.$selected_option = self.element.find('.selected-option');
     self.$options = self.element.find('.options');
 
@@ -12,21 +10,44 @@ $.widget('solasalonstudios.filterabledropdownsearch', {
     * Bind event handlers
     */
     self.$selected_option.on('click', function (e) { self.onClickSelectedOption(e); });
+    self.$options.find('.option').on('click', function (e) { self.onClickOption(e); });
+  },
+
+  close: function () {
+    var self = this;
+
+    self.$options.removeClass('open');
+    $(window).off('click.filterabledropdownsearch');
+  },
+
+  open: function () {
+    var self = this;
+
+    self.$options.addClass('open');
+    $(window).on('click.filterabledropdownsearch', function (e) { self.$options.removeClass('open'); });
+  },
+
+  onClickOption: function (e) {
+    var self = this;
+    var $this = $(e.target);
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    self.$selected_option.find('.name').html($this.html());
+    self.close();
   },
 
   onClickSelectedOption: function (e) {
-    console.log('click');
     var self = this;
 
     e.preventDefault();
     e.stopPropagation();
 
     if (self.$options.hasClass('open')) {
-      self.$options.removeClass('open');
-      $(window).off('click.filterabledropdownsearch');
+      self.close();
     } else {
-      self.$options.addClass('open');
-      $(window).on('click.filterabledropdownsearch', function (e) { self.$options.removeClass('open'); });
+      self.open();
     }
   },
 
