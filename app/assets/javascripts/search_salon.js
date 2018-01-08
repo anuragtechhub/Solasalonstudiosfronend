abeb2b92c0ed70324478acbc3566a3eee4215624
Jeff Bail <jeff@jeffbail.com>
2018-01-08 14:06:26 -0700

    hide phone

--- app/assets/javascripts/search_salon.js
+++ app/assets/javascripts/search_salon.js
@@ -29,7 +29,7 @@ $(function () {
       		html = html + "<p class='general-contact-name'>" + I18n.t('contact_for_leasing_information', {general_contact_name: location.general_contact_name}) + "</p>";
       		html = html + '</div>';
       		
-      		html = html + "<p class='phone-number'><a href='tel:" + location.phone_number + "'>" + I18n.t('phone_result', {phone_number: location.phone_number}) + "</a></p>";
+      		//html = html + "<p class='phone-number'><a href='tel:" + location.phone_number + "'>" + I18n.t('phone_result', {phone_number: location.phone_number}) + "</a></p>";
       		html = html + "<p class='email-address'><a href='mailto:" + location.email_address_for_inquiries + "'>" + I18n.t('email_result', {email_address: location.email_address_for_inquiries}) + "</a></p>";
       		html = html + "<div class='chevron-right'><span class='fa fa-chevron-right'>&nbsp;</span></div>";
 
