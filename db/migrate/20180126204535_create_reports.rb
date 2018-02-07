927a716570ef67a926d72702a722c25c990665bf
Jeff Bail <jeff@jeffbail.com>
2018-02-06 20:03:23 -0600

    add facebook pixel for canada

--- db/migrate/20180126204535_create_reports.rb
+++ db/migrate/20180126204535_create_reports.rb
@@ -1,11 +1,13 @@
 class CreateReports < ActiveRecord::Migration
   def change
-    create_table :reports do |t|
-      t.string :report_type
-      t.string :email_address
-      t.datetime :processed_at
+  	unless ActiveRecord::Base.connection.table_exists? 'reports'
+	    create_table :reports do |t|
+	      t.string :report_type
+	      t.string :email_address
+	      t.datetime :processed_at
 
-      t.timestamps
-    end
+	      t.timestamps
+	    end
+	  end
   end
 end
