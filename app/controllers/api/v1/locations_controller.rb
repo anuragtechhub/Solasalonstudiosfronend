9adab72a00e8eadf29a6920e1fc6e32c9464ce72
Jeff Bail <jeff@jeffbail.com>
2018-02-23 16:14:33 -0700

    set cors headers

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::LocationsController < ApiController
   private
 
   def set_cors_headers
-  	headers['Access-Control-Allow-Origin'] = '*.spydersoft.com'
+  	headers['Access-Control-Allow-Origin'] = '*'
 		headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
 		headers['Access-Control-Request-Method'] = '*'
 		headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
