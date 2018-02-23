81f2cf55e55888a85988852e8599fe7cbd0d05ec
Jeff Bail <jeff@jeffbail.com>
2018-02-23 16:13:59 -0700

    cors

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::LocationsController < ApiController
 
+	before_action :set_cors_headers
+
   def index
     @locations = Location.where(:status => 'open')
     render :json => Oj.dump(@locations.select([:id, :name]))
@@ -9,5 +11,14 @@ class Api::V1::LocationsController < ApiController
     @location = Location.find_by(:id => params[:id])
     render :json => Oj.dump(@location.as_json(:methods => [:salon_professionals], :only => [:id, :name, :general_contact_name, :email_address_for_inquiries, :phone_number]))
   end
+
+  private
+
+  def set_cors_headers
+  	headers['Access-Control-Allow-Origin'] = '*.spydersoft.com'
+		headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+		headers['Access-Control-Request-Method'] = '*'
+		headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	end
 
 end
