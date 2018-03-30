b8264dcd054ac61eb86359b1c3a37a4e27950154
Jeff Bail <jeff@jeffbail.com>
2018-03-30 10:50:17 -0600

    url name uniqueness for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ActiveRecord::Base
   validates :title, :presence => true
   validates :article_url, :presence => true
   validates :location, :presence => true, :if => :franchisee?
-  #validates :url_name, :presence => true, :uniqueness => true
+  validates :url_name, :presence => true, :uniqueness => true
 
   has_attached_file :image, :url => ":s3_alias_url", :path => ":class/:attachment/:id_partition/:style/:filename", :s3_host_alias => 'd3p1kyyvw4qtho.cloudfront.net', :styles => { :full_width => '960#', :directory => '375x375#', :thumbnail => '100x100#' }, :s3_protocol => :https
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
