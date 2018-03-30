0751ddd16f3f9da0035b687552771715952978c8
Jeff Bail <jeff@jeffbail.com>
2018-03-30 13:24:40 -0600

    dont log to console

--- app/controllers/api/v2/locations_controller.rb
+++ app/controllers/api/v2/locations_controller.rb
@@ -5,20 +5,24 @@ class Api::V2::LocationsController < ApiController
   def index
     cache_key = "/api/v2/index/#{Location.order(:updated_at => :desc).first.updated_at}/#{Stylist.order(:updated_at => :desc).first.updated_at}"
     json = Rails.cache.fetch(cache_key) do 
+      p "going in the index cache..."
       @locations = Location.where(:status => 'open').order(:created_at => :asc)
       render_to_string('/api/v2/locations/index', formats: 'json')
     end
-    p "index json=#{json}"
+    #p "index json=#{json}"
+    p "done and returnin json index"
     render :json => json
   end
 
   def show
     cache_key = "/api/v2/show/#{Location.order(:updated_at => :desc).first.updated_at}/#{Stylist.order(:updated_at => :desc).first.updated_at}"
     json = Rails.cache.fetch(cache_key) do 
+      p "going in the show cache..."
       @stylists = Location.find_by(:id => params[:id]).stylists
       render_to_string('/api/v2/locations/show', formats: 'json')
     end
-    p "show json=#{json}"
+    #p "show json=#{json}"
+    p "done and returning json show"
     render :json => json
   end
 
