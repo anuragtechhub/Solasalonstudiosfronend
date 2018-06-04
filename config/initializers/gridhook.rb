21df597379a84dcefdea0f183c943bc799bb8a59
Jeff Bail <jeff@jeffbail.com>
2018-06-04 12:58:54 -0600

    sendgrid event processor

--- config/initializers/gridhook.rb
+++ config/initializers/gridhook.rb
@@ -4,6 +4,7 @@ Gridhook.configure do |config|
 
   config.event_processor = proc do |event|
     # event is a Gridhook::Event object
-    EmailEvent.create! event.attributes
+    p "SENDGRID EVENT PROCESSOR #{event.attributes.inspect}"
+    #EmailEvent.create! event.attributes
   end
 end
