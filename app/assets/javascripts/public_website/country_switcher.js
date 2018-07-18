1786bc71db959876e253287cd7a31b187bcdf596
Jeff Bail <jeff@jeffbail.com>
2018-07-18 16:50:15 -0600

    request domain

--- app/assets/javascripts/public_website/country_switcher.js
+++ app/assets/javascripts/public_website/country_switcher.js
@@ -24,29 +24,31 @@ $(function () {
     return false;
   });
 
-
-
   /*
   * Switch countries when clicking country in country switcher modal
   */
 
   $(document.body).on('click', '.country-switcher-modal .country', function () {
     var $this = $(this);
-    var domain = getDomain(location.href);
 
-    if ($this.data('domain') == 'ca' && domain.startsWith('com')) {
-      window.location.href = "https://www.solasalonstudios.ca" + domain.substring(3, domain.length);
+    console.log('you clicked', $this.data('domain'));
+
+    return;
+    if ($this.data('domain') == 'ca') {
+      window.location.href = "https://www.solasalonstudios.ca" + location.pathname + location.search + location.hash;
     }
 
-    if ($this.data('domain') == 'com' && domain.startsWith('ca')) {
-      window.location.href = "https://www.solasalonstudios.com" + domain.substring(2, domain.length);
+    if ($this.data('domain') == 'com.br') {
+      window.location.href = "https://www.solasalonstudios.com.br" + location.pathname + location.search + location.hash;
+    }
+
+    if ($this.data('domain') == 'com') {
+      window.location.href = "https://www.solasalonstudios.com" + location.pathname + location.search + location.hash;
     }
 
     $('.country-switcher-modal').hide();
     $(document.body).off('click.country-switcher-open');    
   });
-
-
 
   /*
   * Hide open country switcher modal when window resizes
@@ -56,35 +58,5 @@ $(function () {
     $('.country-switcher-modal').hide();
   });
 
-
-
-  /*
-  * Helper Functions
-  */
-
-  var TLDs = ["ac", "ad", "ae", "aero", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "arpa", "as", "asia", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "biz", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cat", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "com", "coop", "cr", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "edu", "ee", "eg", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gov", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "info", "int", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jobs", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mil", "mk", "ml", "mm", "mn", "mo", "mobi", "mp", "mq", "mr", "ms", "mt", "mu", "museum", "mv", "mw", "mx", "my", "mz", "na", "name", "nc", "ne", "net", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "org", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "pro", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "su", "sv", "sy", "sz", "tc", "td", "tel", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr", "travel", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "xn--0zwm56d", "xn--11b5bs3a9aj6g", "xn--3e0b707e", "xn--45brj9c", "xn--80akhbyknj4f", "xn--90a3ac", "xn--9t4b11yi5a", "xn--clchc0ea0b2g2a9gcd", "xn--deba0ad", "xn--fiqs8s", "xn--fiqz9s", "xn--fpcrj9c3d", "xn--fzc2c9e2c", "xn--g6w251d", "xn--gecrj9c", "xn--h2brj9c", "xn--hgbk6aj7f53bba", "xn--hlcj6aya9esc7a", "xn--j6w193g", "xn--jxalpdlp", "xn--kgbechtv", "xn--kprw13d", "xn--kpry57d", "xn--lgbbat1ad8j", "xn--mgbaam7a8h", "xn--mgbayh7gpa", "xn--mgbbh1a71e", "xn--mgbc0a9azcg", "xn--mgberp4a5d4ar", "xn--o3cw4h", "xn--ogbpf8fl", "xn--p1ai", "xn--pgbs0dh", "xn--s9brj9c", "xn--wgbh1c", "xn--wgbl6a", "xn--xkc2al3hye2a", "xn--xkc2dl3a5ee0h", "xn--yfro4i67o", "xn--ygbi2ammx", "xn--zckzah", "xxx", "ye", "yt", "za", "zm", "zw"].join()
-
-  function getDomain(url){
-    var parts = url.split('.');
-    if (parts[0] === 'www' && parts[1] !== 'com'){
-        parts.shift()
-    }
-    var ln = parts.length
-      , i = ln
-      , minLength = parts[parts.length-1].length
-      , part
-
-    // iterate backwards
-    while(part = parts[--i]){
-        // stop when we find a non-TLD part
-        if (i === 0                    // 'asia.com' (last remaining must be the SLD)
-            || i < ln-2                // TLDs only span 2 levels
-            || part.length < minLength // 'www.cn.com' (valid TLD as second-level domain)
-            || TLDs.indexOf(part) < 0  // officialy not a TLD
-        ){
-            return part
-        }
-    }
-  };
 
 });
