90db54e1cb70bb6263809d1913925566d17712f3
Jeff Bail <jeff@jeffbail.com>
2018-07-30 20:30:21 +0200

    incremental work on sola search bar

--- app/assets/javascripts/components/SearchBar/SearchBar.js.jsx
+++ app/assets/javascripts/components/SearchBar/SearchBar.js.jsx
@@ -3,9 +3,18 @@ var SearchBar = React.createClass({
 	render: function () {
 		return (
 			<div className="SearchBar">
-
+				<form onSubmit={this.onSubmit}>
+					<SearchName />
+					<SearchLocation />
+					<SearchDatePicker />
+					<button type="submit" className="primary" /> 
+				</form>
 			</div>
 		);
-	}
+	},
+
+	onSubmit: function () {
+		console.log('submit SearchBar');
+	},
 
 })
--- app/assets/javascripts/components/SearchBar/SearchDatepicker.js.jsx
+++ app/assets/javascripts/components/SearchBar/SearchDatepicker.js.jsx
@@ -3,7 +3,7 @@ var SearchDatePicker = React.createClass({
 	render: function () {
 		return (
 			<div className="SearchDatePicker">
-
+				<input type="text" placeholder="03/17/18" />
 			</div>
 		);
 	}
