0a63e3c9447f8913219306902888c27516ea5ef5
Jeff Bail <jeff@jeffbail.com>
2018-08-31 14:30:47 -0600

    cover image error handler

--- app/assets/javascripts/components/SolaSearch/ProfessionalResult.js.jsx
+++ app/assets/javascripts/components/SolaSearch/ProfessionalResult.js.jsx
@@ -1,16 +1,28 @@
 var ProfessionalResult = React.createClass({
 
+	getInitialState: function () {
+		return {
+			defaultCoverImageUrl: 'https://s3-us-west-2.amazonaws.com/glossgenius-static-v2/user_avatar.jpg',
+			useDefaultCoverImage: false
+		};
+	},
+
 	render: function () {
 		return (
 			<div className="ProfessionalResult">
 				<ProfessionalServicesDropdown services={this.props.all_services} />
-				<div className="ProfessionalCoverImage"><img src={this.props.cover_image} alt={this.props.full_name} /></div>
+				<div className="ProfessionalCoverImage">
+					<img src={this.state.useDefaultCoverImage ? this.state.defaultCoverImageUrl : this.props.cover_image} alt={this.props.full_name} onError={this.onCoverImageError} /></div>
 				<div className="ProfessionalResultDetails">
 					<div className="ProfessionalName">{this.props.full_name}</div>
 					<div className="ProfessionalAddress">{this.props.business_address}</div>
 				</div>
 			</div>
 		);
+	},
+
+	onCoverImageError: function () {
+		this.setState({useDefaultCoverImage: true});
 	},
 
 });
