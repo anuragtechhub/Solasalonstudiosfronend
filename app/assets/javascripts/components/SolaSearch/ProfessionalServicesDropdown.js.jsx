d689c1ee6433eabccd942a17b1def98eed5edce5
Jeff Bail <jeff@jeffbail.com>
2018-08-31 16:24:31 -0600

    professional services dropdown

--- app/assets/javascripts/components/SolaSearch/ProfessionalServicesDropdown.js.jsx
+++ app/assets/javascripts/components/SolaSearch/ProfessionalServicesDropdown.js.jsx
@@ -1,11 +1,23 @@
 var ProfessionalServicesDropdown = React.createClass({
 
+	getInitialState: function () {
+		return {
+			selectedService: this.props.services && this.props.services.length > 0 ? this.props.services[0] : null
+		};
+	},
+
+
+
+	/**
+	* Render functions
+	*/
+
 	render: function () {
-		//console.log('ProfessionalServicesDropdown - this.props.services', this.props.services);
+		var self = this;
 		var services = this.props.services.map(function (service) {
 			return (
 				<li key={service.guid}>
-					<a href="#">{service.name} <strong>${service.price}</strong></a>
+					<a href="#" onClick={self.onSelect.bind(null, service)}>{self.renderService(service)}</a>
 				</li>
 			);
 		});
@@ -14,7 +26,7 @@ var ProfessionalServicesDropdown = React.createClass({
 			<div className="ProfessionalServicesDropdown">
 				<div className="dropdown">
 				  <button className="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
-				    Haircut <strong>$90</strong>
+				    {this.state.selectedService ? this.renderService(this.state.selectedService) : null}
 				    <span className="fa fa-angle-down"></span>
 				  </button>
 				  <ul className="dropdown-menu">
@@ -23,6 +35,37 @@ var ProfessionalServicesDropdown = React.createClass({
 				</div>
 			</div>
 		);
+	},
+
+	renderService: function (service) {
+		return (
+			<span>
+				{service.name} <strong>${this.renderPrice(service.price)}</strong>
+			</span>
+		);
+	},
+
+	renderPrice: function (price) {
+		if (price) {
+			if (price.charAt(price.length - 1) == '+') {
+				return parseInt(price).toFixed(0) + '+';
+			} else {
+				return parseInt(price).toFixed(0);
+			}
+		} else {
+			return price;
+		}
+	},
+
+
+
+	/**
+	* Change handlers	
+	*/
+
+	onSelect: function (service, event) {
+		event.preventDefault();
+		this.setState({selectedService: service});
 	},
 
 });
