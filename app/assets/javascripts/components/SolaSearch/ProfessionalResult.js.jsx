6530786087c54f7aed093fa0e0e9b74654284d7e
Jeff Bail <jeff@jeffbail.com>
2018-09-07 09:56:09 -0600

    availabilities

--- app/assets/javascripts/components/SolaSearch/ProfessionalResult.js.jsx
+++ app/assets/javascripts/components/SolaSearch/ProfessionalResult.js.jsx
@@ -12,7 +12,9 @@ var ProfessionalResult = React.createClass({
 			<div className="ProfessionalResult">
 				<ProfessionalServicesDropdown booking_page_url={this.props.booking_page_url} services={this.props.all_services} />
 				<div className="ProfessionalCoverImage">
-					<img src={this.state.useDefaultCoverImage ? this.state.defaultCoverImageUrl : this.props.cover_image} alt={this.props.full_name} onError={this.onCoverImageError} /></div>
+					<img src={this.state.useDefaultCoverImage ? this.state.defaultCoverImageUrl : this.props.cover_image} alt={this.props.full_name} onError={this.onCoverImageError} />
+					<span className="ProfessionalCoverImageName">{this.props.full_name}</span>
+				</div>
 				<div className="ProfessionalResultDetails">
 					<div className="ProfessionalName">{this.props.full_name}</div>
 					<div className="ProfessionalAddress">{this.props.business_address}</div>
--- app/assets/javascripts/components/SolaSearch/SolaSearch.js.jsx
+++ app/assets/javascripts/components/SolaSearch/SolaSearch.js.jsx
@@ -2,6 +2,7 @@ var SolaSearch = React.createClass({
 
 	getInitialState: function () {
 		return {
+			availabilities: this.props.availabilities || {},
 			date: this.props.date ? moment(this.props.date, "YYYY-MM-DD") : moment(),
 			error: null,
 			fingerprint: this.props.fingerprint,
@@ -15,6 +16,10 @@ var SolaSearch = React.createClass({
 		};
 	},
 
+	componentDidMount: function () {
+		this.getAvailabilities(this.getServicesGuids());
+	},
+
 
 
 	/**
@@ -22,11 +27,11 @@ var SolaSearch = React.createClass({
 	*/
 
 	render: function () {
-		console.log('render SolaSearch locations', this.state.locations);
+		//console.log('render SolaSearch availabilities', this.state.availabilities);
 		
 		return (
 			<div className="SolaSearch">
-				<ProfessionalResults professionals={this.state.professionals} />
+				<ProfessionalResults availabilities={this.state.availabilities} professionals={this.state.professionals} />
 				<LocationsMap lat={this.state.lat} lng={this.state.lng} locations={this.state.locations} zoom={this.state.zoom} />
 			</div>
 		);
@@ -38,12 +43,15 @@ var SolaSearch = React.createClass({
 	* Helper functions
 	*/
 
-	getAvailabilities: function (service_guids) {
+	getAvailabilities: function (services_guids) {
 		var self = this;
 		
+		//console.log('services_guids', JSON.stringify(services_guids));
+
 		$.ajax({
 			data: {
-				service_guids: service_guids
+				date: this.state.date.format("YYYY-MM-DD"),
+				services_guids: services_guids
 			},
 	    headers: {
 	    	"api_key": this.props.gloss_genius_api_key,
@@ -52,8 +60,23 @@ var SolaSearch = React.createClass({
 			method: 'POST',
 	    url: this.props.gloss_genius_api_url + 'availabilities',
 		}).done(function (response) {
-			console.log('getAvailabilities response', response);
+			//console.log('getAvailabilities response', response);
+			self.setState({availabilities: response});
 		}); 
+	},
+
+	getServicesGuids: function () {
+		var guids = {};
+
+		for (var i = 0, ilen = this.state.professionals.length; i < ilen; i++) {
+			guids[this.state.professionals[i].guid] = [];
+			
+			for (var j = 0, jlen = this.state.professionals[i].matched_services.length; j < jlen; j++) {
+				guids[this.state.professionals[i].guid].push(this.state.professionals[i].matched_services[j].guid);
+			}
+		}
+
+		return guids;
 	},
 
 });
