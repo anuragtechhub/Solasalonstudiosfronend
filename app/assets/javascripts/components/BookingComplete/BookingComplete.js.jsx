dd4bbd082f3217d1981aac147636246307b49ce2
Jeff Bail <jeff@jeffbail.com>
2018-10-12 11:25:16 -0600

    add to calendar button

--- app/assets/javascripts/components/BookingComplete/BookingComplete.js.jsx
+++ app/assets/javascripts/components/BookingComplete/BookingComplete.js.jsx
@@ -3,7 +3,18 @@ var BookingComplete = React.createClass({
 	render: function () {
 		return (
 			<div className="BookingComplete">
-				BOOKING COMPLETE!
+				<div className="container">
+					<div className="BookingCompleteHeader">
+						<h2>{I18n.t('sola_search.booking_complete')}</h2>
+						<p>{I18n.t('sola_search.thanks_for_choosing_sola')}</p>
+						<div className="AddToCalendar">
+							<a href="#" className="button primary">{I18n.t('sola_search.add_to_calendar')}</a>
+						</div>
+					</div>
+					<div className="BookingCompleteBox">
+
+					</div>
+				</div>
 			</div>
 		);
 	}
