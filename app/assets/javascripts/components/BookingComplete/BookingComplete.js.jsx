1e6ddb7cc4dd21632d897b559871d541ae0cc586
Jeff Bail <jeff@jeffbail.com>
2018-10-12 15:34:39 -0600

    work on booking complete page

--- app/assets/javascripts/components/BookingComplete/BookingComplete.js.jsx
+++ app/assets/javascripts/components/BookingComplete/BookingComplete.js.jsx
@@ -1,6 +1,15 @@
 var BookingComplete = React.createClass({
+	
+	getInitialState: function () {
+		return {
+			defaultCoverImageUrl: 'https://s3-us-west-2.amazonaws.com/glossgenius-static-v2/user_avatar.jpg',
+			useDefaultCoverImage: false,
+		};
+	},
 
 	render: function () {
+		//console.log('this.props.professional', this.props.professional);
+
 		return (
 			<div className="BookingComplete">
 				<div className="container">
@@ -13,10 +22,50 @@ var BookingComplete = React.createClass({
 					</div>
 					<div className="BookingCompleteBox">
 
+						<div className="Header">
+							<div className="ProfessionalCoverImage">
+								<img src={this.state.useDefaultCoverImage ? this.state.defaultCoverImageUrl : this.props.professional.cover_image} alt={this.props.professional.full_name} onError={this.onCoverImageError} />
+								<div className="Gradient"></div>
+							</div>
+							<div className="ProfessionalInfo">
+								<div className="ProfessionalName">{this.props.professional.full_name}</div>
+								<div className="ProfessionalAddress">{this.props.professional.business_address}</div>
+								<a href={this.props.professional.booking_page_url} target={this.props.professional.booking_page_url} className="Contact">{I18n.t('sola_search.contact')}&nbsp;{this.props.professional.full_name}</a>
+							</div>
+						</div>
+
+						<div className="Body">
+							<div className="DateRow">
+								<span className="fa fa-calendar">&nbsp;</span>
+								<div className="Date">{moment(this.props.time.start).format('MMMM do YYYY')}</div>
+							</div>
+
+							<div className="TimeRow">
+								<span className="fa fa-clock-o">&nbsp;</span>
+								<div className="Date">{moment(this.props.time.start).format('h:mm A')} &ndash; {moment(this.props.time.end).format('h:mm A')}</div>
+							</div>
+
+							<div className="AddressRow">
+								<span className="fa fa-map-marker">&nbsp;</span>
+								<div className="Address">{this.props.professional.business_address}<br /><strong>{this.props.professional.business_name}</strong></div>
+								<a href="#">{I18n.t('sola_search.map_it')}</a>
+							</div>
+						</div>
+
 					</div>
 				</div>
 			</div>
 		);
-	}
+	},
+
+
+
+	/**
+	* Change handlers
+	*/
+
+	onCoverImageError: function () {
+		this.setState({useDefaultCoverImage: true});
+	},
 
 });
