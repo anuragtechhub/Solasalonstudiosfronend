7e9212096b72065e8174b8a55c8cff8f6d9548b5
Jeff Bail <jeff@jeffbail.com>
2018-12-03 14:11:53 -0700

    incremental work on booking referral

--- app/assets/javascripts/components/BookNowReferral.js.jsx
+++ app/assets/javascripts/components/BookNowReferral.js.jsx
@@ -4,6 +4,7 @@ var BookNowReferral = React.createClass({
     return {
       height: 600,
       width: 1200,
+      subdomain: null,
       url: '',
       modalVisible: false,
     };
@@ -47,9 +48,9 @@ var BookNowReferral = React.createClass({
         <h3 className="text-uppercase">Enter your SolaGenius Website</h3>
         <p>Copy the business URL assigned to you through the SolaGenius app and paste it below to receive your referral link.</p>
         <div className="inputs">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="col-input">
-              <input type="text" className="form-control input-block" value={this.state.url} onChange={this.onChangeUrl} onFocus={this.onFocusUrl} onBlur={this.onBlurUrl} placeholder="website.glossgenius.com" />
+              <input type="text" className="form-control input-block" value={this.state.url} onChange={this.onChangeUrl} onFocus={this.onFocusUrl} onBlur={this.onBlurUrl} />
             </div>
             <div className="col-button">
               <button type="submit" className="btn">Get custom referral link</button>
@@ -64,7 +65,17 @@ var BookNowReferral = React.createClass({
     return (
       <div className="found">
         <h3 className="text-uppercase">Your Custom Referral Link is Here!</h3>
-        <p>Copy the business URL assigned to you through the SolaGenius app and paste it below to receive your referral link.</p>
+        <p>Share your link via text, email, or social with friends who should use SolaGenius for their business.</p>
+        <div className="inputs">
+          <form onSubmit={this.onCopy}>
+            <div className="col-input">
+              <input type="text" className="form-control input-block" value={'https://glossgenius.com/invite/' + this.state.subdomain} />
+            </div>
+            <div className="col-button">
+              <button type="submit" className="btn">Copy Link</button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   },
@@ -75,7 +86,7 @@ var BookNowReferral = React.createClass({
         <div className="ink-cloud-left"></div>
         <div className="ink-cloud-right"></div>
         <div className="fa fa-times" onClick={this.onHideModal}></div>
-        {this.validSolaGeniusURL() ? this.renderFrame2() : this.renderFrame1()}
+        {this.state.subdomain && this.state.subdomain != '' ? this.renderFrame2() : this.renderFrame1()}
       </div>
     );
   },
@@ -103,14 +114,28 @@ var BookNowReferral = React.createClass({
     this.setState({modalVisible: false});
   },
 
+  onSubmit: function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var url_regex = /^(?:https*:\/\/)*([a-zA-Z0-9][a-zA-Z0-9-_]*)\.*[a-zA-Z0-9]*[a-zA-Z0-9-_]*glossgenius.com\/*$/igm
+    var matches = url_regex.exec(this.state.url);
+    console.log('onSubmit!', this.state.url, this.state.url.match(url_regex), matches);
+    if (this.state.url.match(url_regex)) {
+      this.setState({subdomain: matches[1]})
+    } else {
+      alert('This does not seem to be a valid SolaGenius website URL.');
+    }
+  },
+
 
 
   /**
   * Helper functions
   */
 
-  validSolaGeniusURL: function () {
-    return false;
-  }
+  // validSolaGeniusURL: function () {
+  //   return false;
+  // }
 
 });
