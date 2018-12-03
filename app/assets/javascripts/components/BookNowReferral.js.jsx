d3e351b08e190ba3c2bb05d5ea0e75a8e09e637b
Jeff Bail <jeff@jeffbail.com>
2018-12-03 11:50:11 -0700

    incremental work on referral modal

---
+++ app/assets/javascripts/components/BookNowReferral.js.jsx
@@ -0,0 +1,84 @@
+var BookNowReferral = React.createClass({
+
+  getInitialState: function () {
+    return {
+      modalVisible: false,
+    };
+  },
+
+  componentDidMount: function () {
+    var self = this;
+
+    window.onShowBookNowReferralModal = function () {
+      self.setState({modalVisible: true});
+      $('body').css('overflow', 'hidden');
+    };
+  },
+
+  render: function () {
+    return (
+      <div className="BookNowReferral">
+        {this.state.modalVisible ? this.renderModal() : null}
+        {this.state.modalVisible ? <div className="BookNowOverlay" onClick={this.onHideModal}>&nbsp;</div> : null}
+      </div>
+    );
+  },
+
+  renderFrame1: function () {
+    return (
+      <div className="found">
+        <h3 className="text-uppercase">Enter your SolaGenius Website</h3>
+        <p>Copy the business URL assigned to you through the SolaGenius app and paste it below to receive your referral link.</p>
+        <div className="inputs">
+          <form>
+            <div className="col-input">
+              <input type="text" className="form-control input-block" />
+            </div>
+            <div className="col-button">
+              <button type="submit" className="btn">Get custom referral link</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  },
+
+  renderFrame2: function () {
+    return (
+      <div className="found">
+        <h3 className="text-uppercase">Your Custom Referral Link is Here!</h3>
+        <p>Copy the business URL assigned to you through the SolaGenius app and paste it below to receive your referral link.</p>
+      </div>
+    );
+  },
+
+  renderModal: function () {
+    return (
+      <div className="BookNowModal">
+        {this.validSolaGeniusURL() ? this.renderFrame2() : this.renderFrame1()}
+      </div>
+    );
+  },
+
+
+
+  /**
+  * Change handlers
+  */
+
+  onHideModal: function () {
+    $('body').css('overflow', 'auto');
+    this.setState({modalVisible: false});
+  },
+
+
+
+  /**
+  * Helper functions
+  */
+
+  validSolaGeniusURL: function () {
+    return false;
+  }
+
+});
