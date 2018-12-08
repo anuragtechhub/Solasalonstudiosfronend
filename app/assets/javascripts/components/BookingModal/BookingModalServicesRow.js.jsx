2c080d0105761def6566e24610e32d6629b128d3
Jeff Bail <jeff@jeffbail.com>
2018-12-08 16:21:20 -0700

    change services

--- app/assets/javascripts/components/BookingModal/BookingModalServicesRow.js.jsx
+++ app/assets/javascripts/components/BookingModal/BookingModalServicesRow.js.jsx
@@ -9,7 +9,7 @@ var BookingModalServicesRow = React.createClass({
 		return (
 			<div className="ServicesRow">
 				<div className="ServiceTitle">{I18n.t(this.props.services.length == 1 ? 'sola_search.service' : 'sola_search.services')} ({this.props.services.length})</div>
-				<a href="#" onClick={this.onChangeServices}>{I18n.t('sola_search.add_remove_services')}</a>
+				<a href="#" onClick={this.onChangeServices}>{I18n.t('sola_search.change_services')}</a>
 
 				<div className="Services">{services}</div>
 			</div>
