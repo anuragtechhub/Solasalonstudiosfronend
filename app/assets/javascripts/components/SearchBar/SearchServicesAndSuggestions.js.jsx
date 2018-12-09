78812d5a76989396cb356e8f456b2c30208402dc
Jeff Bail <jeff@jeffbail.com>
2018-12-08 19:21:23 -0700

    open stylist and salon urls in new window

--- app/assets/javascripts/components/SearchBar/SearchServicesAndSuggestions.js.jsx
+++ app/assets/javascripts/components/SearchBar/SearchServicesAndSuggestions.js.jsx
@@ -150,7 +150,7 @@ var SearchServicesAndSuggestions = React.createClass({
 	renderProfessionals: function () {
 		if (this.state.professionals && this.state.professionals.length > 0) {
 			var professionals = this.state.professionals.map(function (professional) {
-				return <a key={professional.booking_page_url} href={professional.booking_page_url}>{professional.full_name}</a>
+				return <a key={professional.booking_page_url} href={'//' + professional.booking_page_url}>{professional.full_name}</a>
 			});
 
 			return (
@@ -169,7 +169,7 @@ var SearchServicesAndSuggestions = React.createClass({
 	renderSalons: function () {
 		if (this.state.salons && this.state.salons.length > 0) {
 			var salons = this.state.salons.map(function (salon) {
-				return <a key={salon.booking_page_url} href={salon.booking_page_url}>{salon.business_name}</a>
+				return <a key={salon.booking_page_url} href={'//' + salon.booking_page_url}>{salon.business_name}</a>
 			});
 
 			return (
