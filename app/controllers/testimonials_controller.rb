5de8658ee64233ce0b239f473b1f0490e2b1aef8
Jeff Bail <jeff@jeffbail.com>
2019-01-11 13:32:20 -0700

    canada testimonials

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -1,5 +1,8 @@
 class TestimonialsController < PublicWebsiteController
   def index
+  	if I18n.locale.to_s == 'en-CA' 
+  		render 'index_ca'
+  	end
     #@category = BlogCategory.find_by(:url_name => 'stylist_profiles')
     #@related_blogs = Blog.joins(:blog_categories, :blog_blog_categories).where('blog_blog_categories.blog_category_id = ?', @category.id).order(:created_at => :desc).uniq if @category
   end
