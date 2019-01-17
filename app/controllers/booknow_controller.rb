2b34ccd10f3b77dc2629d5583229c0b4a6050330
Jeff Bail <jeff@jeffbail.com>
2019-01-16 18:38:23 -0700

    fix so date is remembered

--- app/controllers/booknow_controller.rb
+++ app/controllers/booknow_controller.rb
@@ -24,7 +24,7 @@ class BooknowController < PublicWebsiteController
 
     begin
       @professionals = JSON.parse(results_response)
-      #@date = params[:date] ? DateTime.parse(params[:date]) : DateTime.now
+      @date = params[:date] ? DateTime.parse(params[:date]) : DateTime.now
       #@locations = Location.near([params[:lat].to_f, params[:lng].to_f], 11)
       @locations = Location.where(:id => get_location_id(@professionals))
       
@@ -41,7 +41,7 @@ class BooknowController < PublicWebsiteController
       p "ERROR WITH THIS CALL - FALLBACK! #{e.inspect}"
       redirect_to booknow_search_path(:date => params[:date], :location => params[:location], :lat => params[:lat], :lng => params[:lng], :query => params[:query]), :flash => { :error => "There was a problem with your search. Please try again." }
       # @professionals = JSON.parse(fallback_results_response)
-      # @date = DateTime.parse(params[:date]) || DateTime.now
+      @date = DateTime.parse(params[:date]) || DateTime.now
       # #@locations = Location.near([params[:lat].to_f, params[:lng].to_f], 11)
       # @locations = Location.where(:id => get_location_id(@professionals))
       
