de275e1a29a3927e447c488a234101f68b48d050
Jeff Bail <jeff@jeffbail.com>
2019-01-18 13:53:20 -0700

    size booking modal images

--- app/assets/javascripts/components/BookingModal/BookingModalReview.js.jsx
+++ app/assets/javascripts/components/BookingModal/BookingModalReview.js.jsx
@@ -4,6 +4,10 @@ var BookingModalReview = React.createClass({
 		return {
 			defaultCoverImageUrl: 'https://s3-us-west-2.amazonaws.com/glossgenius-static-v2/user_avatar.jpg',
 			useDefaultCoverImage: false,
+			height: 'auto',
+			width: '110px',
+			top: '0',
+			left: '0',
 		};
 	},
 
@@ -32,7 +36,7 @@ var BookingModalReview = React.createClass({
 			<div className="Header">
 				<div className="ProfessionalCoverImage">
 					<a href={'//' + this.props.professional.booking_page_url} target={this.props.professional.booking_page_url} className="ga-et" data-gcategory="BookNow" data-gaction="View SolaGenius Professional Landing Page" data-glabel={this.props.professional.booking_page_url}>
-						<img src={this.state.useDefaultCoverImage ? this.state.defaultCoverImageUrl : this.props.professional.cover_image} alt={this.props.professional.full_name} onError={this.onCoverImageError} className="ga-et" data-gcategory="BookNow" data-gaction="View SolaGenius Professional Page" data-glabel={this.props.professional.booking_page_url} />
+						<img style={{height: this.state.height, width: this.state.width, top: this.state.top, left: this.state.left}} src={this.state.useDefaultCoverImage ? this.state.defaultCoverImageUrl : this.props.professional.cover_image} alt={this.props.professional.full_name} onError={this.onCoverImageError} className="ga-et" data-gcategory="BookNow" data-gaction="View SolaGenius Professional Page" data-glabel={this.props.professional.booking_page_url} />
 					</a>
 					<div className="Gradient"></div>
 				</div>
@@ -54,5 +58,67 @@ var BookingModalReview = React.createClass({
 	onCoverImageError: function () {
 		this.setState({useDefaultCoverImage: true});
 	},
+
+	onCoverImageLoad: function (e) {
+		var self = this;
+		//console.log('onCoverImageLoad', $(e.target).attr('src'));
+		//(function () {
+		var img = new Image;
+		img.src = $(e.target).attr('src');
+    img.onload = function() {
+    	var image_scale = self.scaleImage(img.width, img.height, 110, 110);
+    	//console.log("onCoverImageLoad intrinsic size: " + img.width + "x" + img.height, image_scale);
+    	self.setState({top: image_scale.targettop, left: image_scale.targetleft, width: image_scale.width, height: image_scale.height});
+    };
+		//})();
+
+		// console.log('cover image load!', this.width, this.height, $(e.currentTarget).width(), $(e.currentTarget).width());
+	},
+
+
+
+	/**
+	* Helper functions
+	*/
+
+	scaleImage: function (srcwidth, srcheight, targetwidth, targetheight, fLetterBox) {
+    var result = { width: 0, height: 0, fScaleToTargetWidth: true };
+
+    if ((srcwidth <= 0) || (srcheight <= 0) || (targetwidth <= 0) || (targetheight <= 0)) {
+        return result;
+    }
+
+    // scale to the target width
+    var scaleX1 = targetwidth;
+    var scaleY1 = (srcheight * targetwidth) / srcwidth;
+
+    // scale to the target height
+    var scaleX2 = (srcwidth * targetheight) / srcheight;
+    var scaleY2 = targetheight;
+
+    // now figure out which one we should use
+    var fScaleOnWidth = (scaleX2 > targetwidth);
+    if (fScaleOnWidth) {
+        fScaleOnWidth = fLetterBox;
+    }
+    else {
+       fScaleOnWidth = !fLetterBox;
+    }
+
+    if (fScaleOnWidth) {
+        result.width = Math.floor(scaleX1);
+        result.height = Math.floor(scaleY1);
+        result.fScaleToTargetWidth = true;
+    }
+    else {
+        result.width = Math.floor(scaleX2);
+        result.height = Math.floor(scaleY2);
+        result.fScaleToTargetWidth = false;
+    }
+    result.targetleft = Math.floor((targetwidth - result.width) / 2);
+    result.targettop = Math.floor((targetheight - result.height) / 2);
+
+    return result;
+	}
 
 });
