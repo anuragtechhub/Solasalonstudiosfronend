5ff8e8de27a0a266b683d1e8116310e941b5fad7
Jeff Bail <jeff@jeffbail.com>
2019-01-17 17:47:51 -0700

    book now events

--- app/assets/javascripts/components/BookingComplete/BookingComplete.js.jsx
+++ app/assets/javascripts/components/BookingComplete/BookingComplete.js.jsx
@@ -48,7 +48,7 @@ var BookingComplete = React.createClass({
 
 						<div className="Header">
 							<div className="ProfessionalCoverImage">
-								<a href={'//' + this.props.professional.booking_page_url} target={this.props.professional.booking_page_url} className="ga-et" data-gcategory="BookNow" data-gaction="View SolaGenius Professional Page" data-glabel={this.props.professional.booking_page_url}>
+								<a href={'//' + this.props.professional.booking_page_url} target={this.props.professional.booking_page_url} className="ga-et" data-gcategory="BookNow" data-gaction="View SolaGenius Professional Landing Page" data-glabel={this.props.professional.booking_page_url}>
 									<img src={this.state.useDefaultCoverImage ? this.state.defaultCoverImageUrl : this.props.professional.cover_image} alt={this.props.professional.full_name} onError={this.onCoverImageError} />
 								</a>
 								<div className="Gradient"></div>
@@ -56,7 +56,7 @@ var BookingComplete = React.createClass({
 							<div className="ProfessionalInfo">
 								<div className="ProfessionalName">{this.props.professional.full_name}</div>
 								<div className="ProfessionalAddress">{this.props.professional.business_address}</div>
-								<a href={'//' + this.props.professional.booking_page_url + (this.props.professional.booking_page_url.endsWith('/') ? '' : '/') + 'contact'} target={this.props.professional.booking_page_url + 'contact'} className="Contact ga-et" data-gcategory="BookNow" data-gaction="View SolaGenius Contact Page" data-glabel={this.props.professional.booking_page_url + 'contact'}>{I18n.t('sola_search.contact')}&nbsp;{this.props.professional.full_name}</a>
+								<a href={'//' + this.props.professional.booking_page_url + (this.props.professional.booking_page_url.endsWith('/') ? '' : '/') + 'contact'} target={this.props.professional.booking_page_url + 'contact'} className="Contact ga-et" data-gcategory="BookNow" data-gaction="View SolaGenius Professional Contact Page" data-glabel={this.props.professional.booking_page_url + 'contact'}>{I18n.t('sola_search.contact')}&nbsp;{this.props.professional.full_name}</a>
 							</div>
 						</div>
 
