47f1192f6e336b8e6ee6b4ffcfafaf3fdb76468b
Jeff Bail <jeff@jeffbail.com>
2019-02-21 11:35:41 -0700

    request tour inquiry form connected to hubspot

--- app/models/request_tour_inquiry.rb
+++ app/models/request_tour_inquiry.rb
@@ -4,10 +4,35 @@ class RequestTourInquiry < ActiveRecord::Base
   
   belongs_to :location
   belongs_to :visit
-  after_create :send_notification_email
+  after_create :send_notification_email, :sync_with_hubspot
 
   def location_name
     location.display_name if location
+  end
+
+  def sync_with_hubspot
+    p "sync_with_hubspot!"
+
+    if ENV['HUBSPOT_API_KEY'].present?
+      p "HUBSPOT API KEY IS PRESENT, lets sync.."
+
+      Hubspot.configure(hapikey: ENV['HUBSPOT_API_KEY'], portal_id: ENV['HUBSPOT_PORTAL_ID'])
+
+      Hubspot::Form.find("f86ac04f-4f02-4eea-8e75-788023163f9c").submit({
+        email: self.email,
+        name: self.name,
+        phone_number: self.phone,
+        message: self.message,
+        request_url: self.request_url,
+        location_id: self.location_id || '',
+        hs_persona: 'persona_2',
+      })
+    else
+      p "No HUBSPOT API KEY, no sync"
+    end
+  rescue => e
+    # shh...
+    p "error sync_with_hubspot #{e}"
   end
 
   private
