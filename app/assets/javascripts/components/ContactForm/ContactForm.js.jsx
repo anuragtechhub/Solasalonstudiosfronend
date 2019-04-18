50ea0540799d7882a25bb90861bb952c8982295a
Jeff Bail <jeff@jeffbail.com>
2019-04-18 14:41:06 -0600

    incremental work on contact form

--- app/assets/javascripts/components/ContactForm/ContactForm.js.jsx
+++ app/assets/javascripts/components/ContactForm/ContactForm.js.jsx
@@ -2,6 +2,7 @@ var ContactForm = React.createClass({
 
 	getInitialState: function () {
 		return {
+			loading: false,
 			selected_location: this.props.selected_location,
 			selected_location_name: this.props.selected_location_name,
 			selected_state: this.props.selected_state,
@@ -15,7 +16,7 @@ var ContactForm = React.createClass({
 	*/
 
 	render: function () {
-		console.log('render ContactForm', this.props);
+		//console.log('render ContactForm', this.props);
 		
 		return (
 			<div className="contact-form">
@@ -35,16 +36,23 @@ var ContactForm = React.createClass({
 					<div className="form-group">
 						<input className="form-control" name="name" type="text" placeholder={I18n.t("contact_form.your_name")} disabled={!this.state.selected_state || !this.state.selected_location} /> 
 					</div>
+					
 					<div className="form-group">
 						<input className="form-control" name="email" type="text" placeholder={I18n.t("contact_form.email_address")} disabled={!this.state.selected_state || !this.state.selected_location} />
 					</div>
+
 					<div className="form-group"> 
 						<input className="form-control" name="phone" type="text" placeholder={I18n.t("contact_form.phone_number")} disabled={!this.state.selected_state || !this.state.selected_location} /> 
 					</div>
+
 					<div className="form-group">
 						<textarea className="form-control" name="message" placeholder={I18n.t("contact_form.leave_a_message")} disabled={!this.state.selected_state || !this.state.selected_location}></textarea> 
 					</div>
+					
 					<button className="button block primary" disabled={!this.state.selected_state || !this.state.selected_location}>{I18n.t("contact_form.submit_message")}</button>
+					
+					{this.state.loading ? <div className="loading"><div className="spinner">&nbsp;</div></div> : null}
+
 				</form>
 			</div>
 		);
@@ -67,10 +75,23 @@ var ContactForm = React.createClass({
 	}, 
 
 	onSubmit: function (e) {
+		var self = this;
+
 		e.preventDefault();
 		e.stopPropagation();
 
 		console.log('onSubmit yo');
+
+		$.ajax({
+			data: {
+
+			},
+			method: 'POST',
+	    url: 'asdf',
+		}).complete(function (response) {
+			console.log('onSubmit contact form is done', response);
+			
+		}); 
 	},
 
 });
