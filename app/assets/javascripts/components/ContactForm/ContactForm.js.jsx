1cd3b53eeae4698971eb61c18864db403ee26b40
Jeff Bail <jeff@jeffbail.com>
2019-04-19 12:33:05 -0600

    tweak why sola

--- app/assets/javascripts/components/ContactForm/ContactForm.js.jsx
+++ app/assets/javascripts/components/ContactForm/ContactForm.js.jsx
@@ -37,7 +37,7 @@ var ContactForm = React.createClass({
 		//console.log('render ContactForm', this.props);
 		
 		return (
-			<div className="contact-form">
+			<div className={"contact-form " + (this.state.selected_state ? 'full-height' : '')}>
 				<h2>{I18n.t('contact_form.contact_a_sola_near_you')}</h2>
 
 				<form onSubmit={this.onSubmit} disabled={!this.state.selected_state || !this.state.selected_location} ref="form">
