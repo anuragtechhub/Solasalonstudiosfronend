f80c212b68b4644f244495e828489b9b43341b68
Jeff Bail <jeff@jeffbail.com>
2019-04-24 16:23:47 -0600

    newsletter sign up pinned to country

--- app/controllers/newsletter_controller.rb
+++ app/controllers/newsletter_controller.rb
@@ -9,13 +9,14 @@ class NewsletterController < PublicWebsiteController
         # gb.lists.subscribe({:id => '09d9824082', :email => {:email => params[:email]}, :merge_vars => {}, :double_optin => false})
 
         if ENV['HUBSPOT_API_KEY'].present?
-          p "HUBSPOT API KEY IS PRESENT, lets sync.."
+          p "HUBSPOT API KEY IS PRESENT, lets sync...country=#{website_country}"
 
           Hubspot.configure(hapikey: ENV['HUBSPOT_API_KEY'])
 
           Hubspot::Contact.create_or_update!([{
             email: params[:email], 
             hs_persona: 'persona_5',
+            country: website_country
           }])
         else
           p "No HUBSPOT API KEY, no sync"
@@ -33,5 +34,17 @@ class NewsletterController < PublicWebsiteController
   # rescue Gibbon::MailChimpError => e
   #   render :json => {:success => 'Thank you for subscribing!'}
   # end
+
+  private
+
+  def website_country
+    if I18n.locale.to_s == 'en'
+      'United States'
+    elsif I18n.locale.to_s == 'en-CA'
+      'Canada'
+    elsif I18n.locale.to_s == 'pt-BR'
+      'Brazil'
+    end
+  end
 
 end
