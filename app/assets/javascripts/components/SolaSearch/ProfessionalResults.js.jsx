3714d0c06393bbf14d7f4ff822818bb2d818baf1
Jeff Bail <jeff@jeffbail.com>
2019-06-19 17:26:39 -0600

    incremental work on no results page

--- app/assets/javascripts/components/SolaSearch/ProfessionalResults.js.jsx
+++ app/assets/javascripts/components/SolaSearch/ProfessionalResults.js.jsx
@@ -70,11 +70,26 @@ var ProfessionalResults = React.createClass({
 	},
 
 	renderPagination: function () {
-		if (this.props.end_of_results || this.props.professionals.length == 0) {
+		if (this.props.end_of_results && this.props.professionals.length > 0) {
 			//console.log('renderPagination END OF RESULTS');
 			return (
 				<div className="SearchPagination text-center">
 					<em style={{fontSize: 15, color: '#AFAFAF', display: 'block', margin: '30px 0'}}>{I18n.t('sola_search.end_of_results')}</em>
+				</div>
+			);
+		} else if (this.props.professionals.length == 0) {
+			return (
+				<div className="SearchPagination text-center">
+					<div className="no-results">
+						<img className="no-results-icon" src="//solasalonstudios.s3.amazonaws.com/book_now_no_results.png" />
+						<h2>{I18n.t('sola_search.please_try_your_search_again')}</h2>
+						<h3>{I18n.t('sola_search.unable_to_find_results')}</h3>
+						<p>{I18n.t('sola_search.no_results_note_1')}</p>
+						<div>
+							<a href="#" className="btn">{I18n.t('sola_search.contact_your_sola_pro_directly')}</a>
+						</div>
+						<p>{I18n.t('sola_search.no_results_note_2')}</p>
+					</div>
 				</div>
 			);
 		} else {
