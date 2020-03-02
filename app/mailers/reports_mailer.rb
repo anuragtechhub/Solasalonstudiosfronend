1f7264213ba78b0c886809454066f0050b1ab8b9
Matt Hoelzel <alderavellc@gmail.com>
2020-03-02 13:30:46 -0700

    Added alderavell@gmail.com email address to reports

--- app/mailers/reports_mailer.rb
+++ app/mailers/reports_mailer.rb
@@ -12,7 +12,7 @@ class ReportsMailer < ActionMailer::Base
     @location = location
     report_email_address = @location.email_address_for_reports.present? ? @location.email_address_for_reports : @location.email_address_for_inquiries
     attachments["#{@location.url_name}.pdf"] = report_pdf
-    mail(to: 'campbell@solasalonstudios.com', bcc: ['jeff@jeffbail.com'], subject: "PageView Drop Alert for #{@location.name}")
+    mail(to: 'campbell@solasalonstudios.com', bcc: ['jeff@jeffbail.com', 'alderavellc@gmail.com'], subject: "PageView Drop Alert for #{@location.name}")
   end
 
   def send_report(email_address, subject, csv_file)
@@ -23,31 +23,31 @@ class ReportsMailer < ActionMailer::Base
   def booknow_report(email_address, report_pdf)
     attachments["booknow.pdf"] = report_pdf
     #mail(to: ['jeff@jeffbail.com'], bcc: ['jeff@jeffbail.com'], subject: "Sola Welcome Email Report")
-    mail(to: email_address, bcc: ['jeff@jeffbail.com'], subject: "BookNow Report")
+    mail(to: email_address, bcc: ['jeff@jeffbail.com', 'alderavellc@gmail.com'], subject: "BookNow Report")
   end
 
   def booking_complete_report(email_address, report_pdf)
     attachments["booking_complete.pdf"] = report_pdf
     #mail(to: ['jeff@jeffbail.com'], bcc: ['jeff@jeffbail.com'], subject: "Sola Welcome Email Report")
-    mail(to: email_address, bcc: ['jeff@jeffbail.com'], subject: "Booking Complete Report")
+    mail(to: email_address, bcc: ['jeff@jeffbail.com', 'alderavellc@gmail.com'], subject: "Booking Complete Report")
   end
 
   def welcome_email_report(report_pdf)
     attachments["welcome_email_report.pdf"] = report_pdf
     #mail(to: ['jeff@jeffbail.com'], bcc: ['jeff@jeffbail.com'], subject: "Sola Welcome Email Report")
-    mail(to: ['jennie@solasalonstudios.com', 'megan@solasalonstudios.com'], bcc: ['jeff@jeffbail.com'], subject: "Sola Welcome Email Report")
+    mail(to: ['jennie@solasalonstudios.com', 'megan@solasalonstudios.com'], bcc: ['jeff@jeffbail.com', 'alderavellc@gmail.com'], subject: "Sola Welcome Email Report")
   end
 
   def location_contact_form_submission_report(email_addresses, csv_file, start_date, end_date)
     attachments['report.csv'] = csv_file
-    mail(to: email_addresses, bcc: ['jeff@jeffbail.com'], subject: "Location Contact Form Submissions from #{start_date.strftime('%B %Y')} to #{end_date.strftime('%B %Y')}")
+    mail(to: email_addresses, bcc: ['jeff@jeffbail.com', 'alderavellc@gmail.com'], subject: "Location Contact Form Submissions from #{start_date.strftime('%B %Y')} to #{end_date.strftime('%B %Y')}")
   end
 
   def rent_manager_locations(email_addresses, summary, locations_csv)
     if email_addresses.present?
       @summary = summary
       attachments['locations.csv'] = locations_csv
-      mail(to: email_addresses, bcc: ['jeff@jeffbail.com'], subject: "Rent Manager Location Match Task Summary")
+      mail(to: email_addresses, bcc: ['jeff@jeffbail.com', 'alderavellc@gmail.com'], subject: "Rent Manager Location Match Task Summary")
     end
   end
 end
