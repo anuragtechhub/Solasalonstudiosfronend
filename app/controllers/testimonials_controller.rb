a96f2cce2b243bcb645554abd8b406d94ccdb6d3
alderavellc <alderavellc@gmail.com>
2020-04-10 14:45:40 -0600

    Update testimonials for Brazil

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -2,6 +2,9 @@ class TestimonialsController < PublicWebsiteController
   def index
   	if I18n.locale.to_s == 'en-CA' 
   		render 'index_ca'
+    elsif
+    I18n.locale.to_s == 'pt-BR' 
+      render 'index_br'
   	end
     #@category = BlogCategory.find_by(:url_name => 'stylist_profiles')
     #@related_blogs = Blog.joins(:blog_categories, :blog_blog_categories).where('blog_blog_categories.blog_category_id = ?', @category.id).order(:created_at => :desc).uniq if @category
