3e5b040af75a09f6bac68c80cfd42e4b0fc5cd8e
Serge White <hard.sintex@gmail.com>
2020-10-11 13:43:25 +0300

    Small update

--- db/migrate/20201010122444_migrate_from_sg_booking_url.rb
+++ db/migrate/20201010122444_migrate_from_sg_booking_url.rb
@@ -1,6 +1,6 @@
 class MigrateFromSgBookingUrl < ActiveRecord::Migration
   def change
-    Stylist.where.not(sg_booking_url:'').find_each {|st| st.update_attribute(:booking_url, st.sg_booking_url)}
+    Stylist.where.not(sg_booking_url:'').find_each {|st| st.update_attribute(:booking_url, st.sg_booking_url) if st.sg_booking_url.presesnt? }
     # remove_column :stylists, :sg_booking_url
   end
 end
