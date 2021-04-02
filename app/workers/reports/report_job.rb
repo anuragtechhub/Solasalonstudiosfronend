98ef1bd1856a4d72893489a9f3f3ef637f302cf8
Serge White <hard.sintex@gmail.com>
2021-04-02 16:07:34 +0300

    Improve worker

--- app/workers/reports/report_job.rb
+++ app/workers/reports/report_job.rb
@@ -9,7 +9,7 @@ module Reports
 
     sidekiq_options(
       queue: :reports,
-      retry: 5,
+      retry: 10,
       unique: :until_executed,
       unique_expiration: 3.days,
       backtrace: true
@@ -31,8 +31,10 @@ module Reports
     sidekiq_retry_in do |count, exception|
       case exception
       when Google::Apis::RateLimitError
-        60 * 60 * 24
+        60 * 60 * 15
         #10 * (count + 1) # (i.e. 10, 20, 30, 40, 50)
+      else
+        60 * 60 * 3
       end
     end
   end
