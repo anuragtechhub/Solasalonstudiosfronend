e14ba04a71e6ff204f28de537c55b41a44a8efc8
Serge White <hard.sintex@gmail.com>
2021-05-30 08:12:41 +0300

    [SSS-162] cache for v1 https://bbac.atlassian.net/browse/SSS-162

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -4,12 +4,18 @@ class Api::V1::LocationsController < ApiController
   before_action :set_cache_headers
 
   def index
-    render json: Oj.dump(Location.open.select([:id, :name]).as_json)
+    json = Rails.cache.fetch("/api/v1/index/#{Location.maximum(:updated_at).to_i}", expires_in: 12.hours) do
+      render_to_string json: Oj.dump(Location.open.select([:id, :name]).as_json)
+    end
+    render json: json
   end
 
   def show
-    render json: Oj.dump(Location.find(params[:id]).
-      as_json(methods: [:salon_professionals],
-              only: [:id, :name, :general_contact_name, :email_address_for_inquiries, :phone_number]))
+    json = Rails.cache.fetch("/api/v1/show/#{params[:id]}/#{Stylist.where(location_id: params[:id]).not_reserved.maximum(:updated_at).to_i}", expires_in: 12.hours) do
+      render_to_string json: Oj.dump(Location.find(params[:id]).
+        as_json(methods: [:salon_professionals],
+                only: [:id, :name, :general_contact_name, :email_address_for_inquiries, :phone_number]))
+    end
+    render json: json
   end
 end
