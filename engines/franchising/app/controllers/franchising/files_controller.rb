6221369d9e249f1d0d1c99756d6fb1b0f6504abf
Serge White <hard.sintex@gmail.com>
2021-09-24 21:02:20 +0300

    [SSS-209] Sola Franchise Updates
    https://bbac.atlassian.net/browse/SSS-209

--- engines/franchising/app/controllers/franchising/files_controller.rb
+++ engines/franchising/app/controllers/franchising/files_controller.rb
@@ -3,7 +3,8 @@ require_dependency 'franchising/application_controller'
 module Franchising
 	class FilesController < ApplicationController
 		def pdf_guide
-			source_name = if canadian_locale?
+			locale = params[:country].presence || I18n.locale.to_s
+			source_name = if locale == 'ca'
 				'Sola_Franchise_Brochure_CAN_9.1.21_Final.pdf'
 			else
 				'Sola Salon Studios Franchise Guide.pdf'
