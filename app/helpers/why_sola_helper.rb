46eb4c047d0d48dcf7f7c804e8ffd0d0aeaf0960
Serge White <hard.sintex@gmail.com>
2021-10-02 00:06:43 +0300

    Fix sidebar

--- app/helpers/why_sola_helper.rb
+++ app/helpers/why_sola_helper.rb
@@ -1,11 +1,11 @@
 module WhySolaHelper
   def why_sola_sections
     result = %i[why_sola sola_pro]
-    result << :solagenius unless canadian_locale?
-    result << :booknow unless canadian_locale?
+    result << :solagenius if english_locale?
+    result << :booknow if english_locale?
     result << :education
-    result << :beauty_hive unless canadian_locale?
-    result << :the_sola_store unless canadian_locale?
+    result << :beauty_hive if english_locale?
+    result << :the_sola_store if english_locale?
     result
   end
 end
