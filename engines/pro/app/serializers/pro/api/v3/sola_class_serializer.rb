6dca115f7690b34a269045f2a3f69b5b7e6b2826
Serge White <hard.sintex@gmail.com>
2021-12-17 16:02:36 +0200

    Add email and phone number to SolaClass serializer

--- engines/pro/app/serializers/pro/api/v3/sola_class_serializer.rb
+++ engines/pro/app/serializers/pro/api/v3/sola_class_serializer.rb
@@ -3,7 +3,8 @@ module Pro
     attributes :id, :title, :image_url, :file_url, :description,
                      :start_date, :start_time,
                      :link_text, :link_url,
-                     :address, :location, :city, :state, :cost, :views
+                     :address, :location, :city, :state, :cost, :views,
+                     :rsvp_email_address, :rsvp_phone_number
 
     belongs_to :video
     belongs_to :category
