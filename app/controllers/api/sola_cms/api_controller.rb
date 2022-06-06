08856a78ffc300c9d22185beb71b8fb74fbae25d
rahulrailsgeek <104260771+rahulrailsgeek@users.noreply.github.com>
2022-06-06 18:13:25 +0530

    fixed the cors error for sola-cms api (#76)

--- app/controllers/api/sola_cms/api_controller.rb
+++ app/controllers/api/sola_cms/api_controller.rb
@@ -1,10 +1,17 @@
 class Api::SolaCms::ApiController < ActionController::Base
-  before_action :restrict_api_access
+  before_action :restrict_api_access, :set_cors_headers
   
   protected
 
   def restrict_api_access
     account = Account.find_by(api_key: request.headers["api-key"])
     render json: { api_key: ['invalid'] }, status: :unprocessable_entity unless account
+  end
+
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
